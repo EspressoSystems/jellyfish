552f83b32d247020b4046ed153dc23e2d7b90438
zhenfei <zhenfei.zhang@hotmail.com>
2022-02-23 07:47:07 -0500

    address comments

--- plonk/examples/proof_of_exp.rs
+++ plonk/examples/proof_of_exp.rs
@@ -13,12 +13,12 @@
 
 use ark_bls12_381::Bls12_381;
 use ark_ec::{
-    twisted_edwards_extended::GroupAffine as TEAffine, AffineCurve, ModelParameters,
-    ProjectiveCurve, TEModelParameters,
+    twisted_edwards_extended::GroupAffine as TEAffine, AffineCurve, PairingEngine, ProjectiveCurve,
+    TEModelParameters,
 };
-use ark_ed_on_bls12_381::{EdwardsAffine, Fr};
+use ark_ed_on_bls12_381::{EdwardsAffine, EdwardsParameters, Fr};
 use ark_ff::PrimeField;
-use ark_std::{test_rng, UniformRand};
+use ark_std::{rand::SeedableRng, UniformRand};
 use jf_plonk::{
     circuit::{customized::ecc::Point, Circuit, PlonkCircuit},
     errors::PlonkError,
@@ -26,12 +26,13 @@ use jf_plonk::{
     transcript::StandardTranscript,
 };
 use jf_utils::fr_to_fq;
+use rand_chacha::ChaCha20Rng;
 
 // The following example proves knowledge of exponent.
 #[allow(non_snake_case)]
 fn main() -> Result<(), PlonkError> {
     // set up the inputs and parameters
-    let mut rng = test_rng();
+    let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
     let x = Fr::rand(&mut rng);
     let G = EdwardsAffine::prime_subgroup_generator();
     let X = G.mul(x).into_affine();
@@ -42,12 +43,12 @@ fn main() -> Result<(), PlonkError> {
     // - public group element `X := xG`
     // This circuit does not need to have real inputs.
     // We can simply use a dummy data set.
-    let circuit = proof_of_exponent_circuit(x, X)?;
+    let circuit = proof_of_exponent_circuit::<EdwardsParameters, Bls12_381, _>(x, X)?;
 
     // Knowing the circuit size, we are able to simulate the universal
     // setup and obtain the structured reference string (SRS).
     //
-    let circuit_size = circuit.num_gates() + 100; // adding 100 as a buffer
+    let circuit_size = circuit.num_gates() + 2; // adding 100 as a buffer
     let srs = PlonkKzgSnark::<Bls12_381>::universal_setup(circuit_size, &mut rng)?;
 
     // Then, we generate the proving key and verification key from the SRS and
@@ -63,7 +64,8 @@ fn main() -> Result<(), PlonkError> {
 
     // Last step, verify the proof against the public inputs.
     let public_inputs = circuit.public_input().unwrap();
-    // extra messages to bound to proof by appending in its transcripts, not used here.
+    // extra messages to bound to proof by appending in its transcripts, not used
+    // here.
     let extra_transcript_init_msg = None;
     assert!(PlonkKzgSnark::<Bls12_381>::verify::<StandardTranscript>(
         &vk,
@@ -80,47 +82,51 @@ fn main() -> Result<(), PlonkError> {
 //
 // We write the code with generics so that is can be adapted to
 // multiple curves.
-// Specifically, the PoE is associated with an embedded curve with param `P`,
-// that defined twisted-edwards parameters for a curve.
+// Specifically, the PoE is associated with
+// - an embedded curve with param `EmbedCurve` that defined twisted-edwards
+//   parameters for a curve
+// - a pairing engine
+// - the native field F for the prove system
 #[allow(non_snake_case)]
-fn proof_of_exponent_circuit<P>(
-    x: P::ScalarField,
-    X: TEAffine<P>,
-) -> Result<PlonkCircuit<P::BaseField>, PlonkError>
+fn proof_of_exponent_circuit<EmbedCurve, PairingCurve, F>(
+    x: EmbedCurve::ScalarField,
+    X: TEAffine<EmbedCurve>,
+) -> Result<PlonkCircuit<EmbedCurve::BaseField>, PlonkError>
 where
-    P: TEModelParameters + Clone,
-    <P as ModelParameters>::BaseField: PrimeField,
+    F: PrimeField,
+    EmbedCurve: TEModelParameters<BaseField = F> + Clone,
+    PairingCurve: PairingEngine<Fr = F>,
 {
     // Let's check that the inputs are indeed correct before we build a circuit.
-    let G = TEAffine::<P>::prime_subgroup_generator();
+    let G = TEAffine::<EmbedCurve>::prime_subgroup_generator();
     assert_eq!(X, G.mul(x), "the inputs are incorrect: X != xG");
 
     // Step 1:
     // We instantiate a turbo plonk circuit.
     //
     // Here we only need turbo plonk since we are not using plookups.
-    let mut circuit = PlonkCircuit::<P::BaseField>::new_turbo_plonk();
+    let mut circuit = PlonkCircuit::<F>::new_turbo_plonk();
 
     // Step 2:
     // now we create variables for each input to the circuit.
 
     // First variable is x which is an field element over P::ScalarField.
     // We will need to lift it to P::BaseField.
-    let x_fq = fr_to_fq::<_, P>(&x);
+    let x_fq = fr_to_fq::<_, EmbedCurve>(&x);
     let x_var = circuit.create_variable(x_fq)?;
 
     // The next variable is a public constant: generator `G`.
     // We need to convert the point to Jellyfish's own `Point` struct.
-    let G_jf: Point<P::BaseField> = G.into();
+    let G_jf: Point<F> = G.into();
     let G_var = circuit.create_constant_point_variable(G_jf)?;
 
     // The last variable is a public variable `X`.
-    let X_jf: Point<P::BaseField> = X.into();
+    let X_jf: Point<F> = X.into();
     let X_var = circuit.create_public_point_variable(X_jf)?;
 
     // Step 3:
     // Connect the wires.
-    let X_var_computed = circuit.variable_base_scalar_mul::<P>(x_var, &G_var)?;
+    let X_var_computed = circuit.variable_base_scalar_mul::<EmbedCurve>(x_var, &G_var)?;
     circuit.point_equal_gate(&X_var_computed, &X_var)?;
 
     // Sanity check: the circuit must be satisfied.
