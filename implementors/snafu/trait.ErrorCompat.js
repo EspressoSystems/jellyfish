a2e38d9c0be9ba672f3d57d01119a404f88e4cf6
alxiong <alxiong@users.noreply.github.com>
2022-02-26 09:20:15 +0000

    deploy: be58b5d869b026ccd5033584792d6afaa3119d9f

---
+++ implementors/snafu/trait.ErrorCompat.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["jf_utils"] = [{"text":"impl ErrorCompat for <a class=\"enum\" href=\"jf_utils/enum.TaggedBlobError.html\" title=\"enum jf_utils::TaggedBlobError\">TaggedBlobError</a>","synthetic":false,"types":["jf_utils::serialize::TaggedBlobError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
