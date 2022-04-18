7fc117944190092d670b500a87273660edc1065c
zhenfeizhang <zhenfeizhang@users.noreply.github.com>
2022-04-18 21:14:07 +0000

    deploy: db8c0abd575b67d51681dd8e24b4d8f0ffc788f3

--- jf_primitives/schnorr_dsa/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["KeyPair","Signature secret key pair used to sign messages"],["Signature","The signature of Schnorr signature scheme"],["VerKey","Signature public verification key"]]});
