b6370669126426658ef4c58e03306bf3c9e45697
alxiong <alxiong@users.noreply.github.com>
2022-05-14 04:51:52 +0000

    deploy: 889180bb03c7eedc99928e6d2f9e2be83a0369ad

--- jf_primitives/hash_to_group/sidebar-items.js
+++ jf_primitives/hash_to_group/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["HashToGroup","Trait definition and default implementation for hash to group functions."]]});
+initSidebarItems({"trait":[["SWHashToGroup","Trait definition and default implementation for hash to group functions for Short Weierstrass Curves."],["TEHashToGroup","Trait definition and default implementation for hash to group functions for Twisted Edwards Curves."]]});
