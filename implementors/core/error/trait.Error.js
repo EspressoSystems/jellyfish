14c2186e826e56e8744cb42e495cd8de8e516a4a
tessico <tessico@users.noreply.github.com>
2022-12-08 14:51:15 +0000

    deploy: 9b7beb26360fb72eb80259330e37aa942e3bf854

--- implementors/core/error/trait.Error.js
+++ implementors/core/error/trait.Error.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"jf_plonk":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"jf_plonk/errors/enum.PlonkError.html\" title=\"enum jf_plonk::errors::PlonkError\">PlonkError</a>"]]
+"jf_plonk":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"jf_plonk/errors/enum.PlonkError.html\" title=\"enum jf_plonk::errors::PlonkError\">PlonkError</a>"]],
+"jf_primitives":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"jf_primitives/errors/enum.PrimitivesError.html\" title=\"enum jf_primitives::errors::PrimitivesError\">PrimitivesError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ jf_primitives/rescue/sponge/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["RescueCRHF","CRHF"],["RescuePRF","PRF"]]};
