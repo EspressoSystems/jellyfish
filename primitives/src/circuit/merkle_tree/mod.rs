8fc260af0b67239cd9f34016bd56578e8011733b
tessico <tessico@protonmail.com>
2022-12-13 15:08:48 +0100

    Add a doctest for MerkleTreeGadget

--- primitives/src/circuit/merkle_tree/mod.rs
+++ primitives/src/circuit/merkle_tree/mod.rs
@@ -21,7 +21,31 @@ pub struct LeafVar {
     pub elem: Variable,
 }
 
-/// Gadgets for rescue-based merkle tree
+/// Gadget for a Merkle tree
+///
+/// # Examples
+///
+/// ```
+/// use ark_bls12_377::Fq;
+/// use jf_primitives::circuit::merkle_tree::MerkleTreeGadget;
+/// use jf_relation::{Circuit, PlonkCircuit};
+/// use jf_primitives::merkle_tree::{prelude::RescueMerkleTree, MerkleTreeScheme, MerkleCommitment};
+///
+/// let mut circuit = PlonkCircuit::<Fq>::new_turbo_plonk();
+/// // Create a 3-ary MT, instantiated with a Rescue-based hash, of height 1.
+/// let elements = vec![Fq::from(1_u64), Fq::from(2_u64), Fq::from(100_u64)];
+/// let mt = RescueMerkleTree::<Fq>::from_elems(1, elements).unwrap();
+/// let expected_root = mt.commitment().digest();
+/// // Get a proof for the element in position 2
+/// let (_, proof) = mt.lookup(2).expect_ok().unwrap();
+///
+/// // Circuit computation with a MT
+/// let leaf_var = circuit.create_leaf_variable(Fq::from(2_u64), Fq::from(100_u64)).unwrap();
+/// let path_vars = circuit.create_membership_proof_variable(&proof).unwrap();
+/// let root_var = circuit.create_root_variable(expected_root).unwrap();
+/// circuit.enforce_merkle_proof(leaf_var, path_vars, root_var).unwrap();
+/// assert!(circuit.check_circuit_satisfiability(&[]).is_ok());
+/// ```
 pub trait MerkleTreeGadget<F, M>
 where
     F: PrimeField,
