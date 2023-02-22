ea24b238ed23e760b9ec2f404330034ca7b34694
tessico <tessico@protonmail.com>
2023-02-22 15:29:37 +0100

    replace usages of `(de)serialize` with `(de)serialize_compressed`

--- utilities/src/serialize.rs
+++ utilities/src/serialize.rs
@@ -18,7 +18,7 @@ pub struct CanonicalBytes(pub Vec<u8>);
 impl<T: ark_serialize::CanonicalSerialize> From<T> for CanonicalBytes {
     fn from(obj: T) -> CanonicalBytes {
         let mut bytes = Vec::new();
-        obj.serialize(&mut bytes)
+        obj.serialize_compressed(&mut bytes)
             .expect("fail to serialize to canonical bytes");
         CanonicalBytes(bytes)
     }
@@ -65,7 +65,7 @@ pub mod canonical {
         serializer: S,
     ) -> Result<S::Ok, S::Error> {
         let mut bytes = Vec::new();
-        T::serialize(elem, &mut bytes).map_err(|e| S::Error::custom(format!("{e:?}")))?;
+        T::serialize_compressed(elem, &mut bytes).map_err(|e| S::Error::custom(format!("{e:?}")))?;
         Serialize::serialize(&TaggedBase64::new("FIELD", &bytes).unwrap(), serializer)
     }
 
@@ -74,7 +74,7 @@ pub mod canonical {
     ) -> Result<T, D::Error> {
         let tb64 = <TaggedBase64 as Deserialize>::deserialize(deserializer)?;
         if tb64.tag() == "FIELD" {
-            T::deserialize(tb64.as_ref()).map_err(|e| D::Error::custom(format!("{e:?}")))
+            T::deserialize_compressed_unchecked(tb64.as_ref()).map_err(|e| D::Error::custom(format!("{e:?}")))
         } else {
             Err(D::Error::custom(format!(
                 "incorrect tag (expected FIELD, got {})",
