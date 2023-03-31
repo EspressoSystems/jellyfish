2ba50b4cd3998b1baa6b4f354cb4e67232e5ac64
Philippe Camacho <philippe@espressosys.com>
2023-03-31 11:01:42 -0300

    Remove type parameter P:Pairing.

--- primitives/src/signatures/bls_arkwors.rs
+++ primitives/src/signatures/bls_arkwors.rs
@@ -9,7 +9,7 @@
 use super::SignatureScheme;
 use ark_bn254::{
     g1::{G1_GENERATOR_X, G1_GENERATOR_Y},
-    Fq, Fr, G1Affine,
+    Bn254, Fq as BaseField, Fr as ScalarField, G1Affine, G1Projective, G2Affine, G2Projective,
 };
 
 use crate::{
@@ -34,7 +34,6 @@ use ark_ff::{
 use ark_serialize::*;
 use ark_std::{
     hash::{Hash, Hasher},
-    marker::PhantomData,
     rand::{CryptoRng, Rng, RngCore},
     string::ToString,
     vec::Vec,
@@ -50,27 +49,24 @@ use tagged_base64::tagged;
 use zeroize::Zeroize;
 
 /// BLS signature scheme.
-pub struct BLSOverBNCurveSignatureScheme<P> {
-    curve_param: PhantomData<P>, // TODO what is this?
+pub struct BLSOverBNCurveSignatureScheme {
+    // curve_param: PhantomData<P>, // TODO what is this?
 }
 
-impl<P> SignatureScheme for BLSOverBNCurveSignatureScheme<P>
-where
-    P: Pairing + Zeroize + Default,
-{
+impl SignatureScheme for BLSOverBNCurveSignatureScheme {
     const CS_ID: &'static str = CS_ID_BLS_MIN_SIG; // TODO change this
 
     /// Signing key.
-    type SigningKey = SignKey<P>;
+    type SigningKey = SignKey;
 
     /// Verification key
-    type VerificationKey = VerKey<P>;
+    type VerificationKey = VerKey;
 
     /// Public Parameter
     type PublicParameter = ();
 
     /// Signature
-    type Signature = Signature<P>;
+    type Signature = Signature;
 
     /// A message is &\[MessageUnit\]
     type MessageUnit = u8;
@@ -87,7 +83,7 @@ where
         _pp: &Self::PublicParameter,
         prng: &mut R,
     ) -> Result<(Self::SigningKey, Self::VerificationKey), PrimitivesError> {
-        let kp = KeyPair::<P>::generate(prng);
+        let kp = KeyPair::generate(prng);
         Ok((kp.sk, kp.vk))
     }
 
@@ -100,7 +96,7 @@ where
     ) -> Result<Self::Signature, PrimitivesError> {
         // TODO
         Ok(Signature {
-            sigma: P::G1::generator(),
+            sigma: G1Projective::generator(),
         })
     }
 
@@ -123,15 +119,15 @@ where
     Clone, Hash, Default, Zeroize, Eq, PartialEq, CanonicalSerialize, CanonicalDeserialize, Debug,
 )]
 /// Signing key for BLS signature.
-pub struct SignKey<P: Pairing>(pub(crate) P::ScalarField);
+pub struct SignKey(pub(crate) ScalarField);
 
-impl<P: Pairing> Drop for SignKey<P> {
+impl Drop for SignKey {
     fn drop(&mut self) {
         self.0.zeroize();
     }
 }
 
-impl<P: Pairing> SignKey<P> {
+impl SignKey {
     // returns the randomized key
     // fn randomize_with(&self, randomizer: &F) -> Self {
     //     Self(self.0 + randomizer)
@@ -145,18 +141,10 @@ impl<P: Pairing> SignKey<P> {
 /// Signature public verification key
 // derive zeroize here so that keypair can be zeroized
 #[tagged(tag::BLS_VER_KEY)] // TODO how does this work???
-#[derive(CanonicalSerialize, CanonicalDeserialize, Derivative)]
-#[derivative(
-    Debug(bound = "P: Pairing"),
-    Default(bound = "P: Pairing"),
-    Eq(bound = "P: Pairing"),
-    Clone(bound = "P: Pairing")
-)]
-pub struct VerKey<P>(pub(crate) P::G2)
-where
-    P: Pairing;
+#[derive(CanonicalSerialize, CanonicalDeserialize, Eq, Clone, Debug)]
+pub struct VerKey(pub(crate) G2Projective);
 
-impl<P: Pairing> VerKey<P> {
+impl VerKey {
     // TODO is this needed?
     // Return a randomized verification key.
     // pub fn randomize_with<F>(&self, randomizer: &F) -> Self
@@ -169,19 +157,13 @@ impl<P: Pairing> VerKey<P> {
     // }
 }
 
-impl<P> Hash for VerKey<P>
-where
-    P: Pairing,
-{
+impl Hash for VerKey {
     fn hash<H: Hasher>(&self, state: &mut H) {
         Hash::hash(&self.0.into_affine(), state)
     }
 }
 
-impl<P> PartialEq for VerKey<P>
-where
-    P: Pairing,
-{
+impl PartialEq for VerKey {
     fn eq(&self, other: &Self) -> bool {
         self.0.into_affine().eq(&other.0.into_affine())
     }
@@ -205,9 +187,9 @@ where
 //     }
 // }
 
-impl<P: Pairing> VerKey<P> {
+impl VerKey {
     /// Convert the verification key into the affine form.
-    pub fn to_affine(&self) -> P::G2Affine {
+    pub fn to_affine(&self) -> G2Affine {
         self.0.into_affine()
     }
 }
@@ -219,19 +201,11 @@ impl<P: Pairing> VerKey<P> {
 /// Signature secret key pair used to sign messages
 // make sure sk can be zeroized
 #[tagged(tag::SCHNORR_KEY_PAIR)] // TODO what is this tag for?
-#[derive(CanonicalSerialize, CanonicalDeserialize, Derivative)]
-#[derivative(
-    Debug(bound = "P: Pairing"),
-    Clone(bound = "P: Pairing"),
-    PartialEq(bound = "P: Pairing")
-)]
-pub struct KeyPair<P>
-where
-    P: Pairing,
-{
-    phantom: PhantomData<P>,
-    sk: SignKey<P>,
-    vk: VerKey<P>,
+#[derive(CanonicalSerialize, CanonicalDeserialize, Clone)]
+pub struct KeyPair {
+    // phantom: PhantomData,
+    sk: SignKey,
+    vk: VerKey,
 }
 
 // impl<P> Default for KeyPair<P>
@@ -252,34 +226,19 @@ where
 
 /// The signature of BLS signature scheme
 #[tagged(tag::SCHNORR_SIG)] // TODO what is this tag for?
-#[derive(CanonicalSerialize, CanonicalDeserialize, Derivative)]
-#[derivative(
-    Debug(bound = "P: Pairing"),
-    Default(bound = "P: Pairing"),
-    Eq(bound = "P: Pairing"),
-    Clone(bound = "P: Pairing")
-)]
+#[derive(CanonicalSerialize, CanonicalDeserialize, Eq, Clone, Debug)]
 #[allow(non_snake_case)]
-pub struct Signature<P>
-where
-    P: Pairing,
-{
-    pub(crate) sigma: P::G1,
+pub struct Signature {
+    pub(crate) sigma: G1Projective,
 }
 
-impl<P> Hash for Signature<P>
-where
-    P: Pairing,
-{
+impl Hash for Signature {
     fn hash<H: Hasher>(&self, state: &mut H) {
         Hash::hash(&self.sigma, state);
     }
 }
 
-impl<P> PartialEq for Signature<P>
-where
-    P: Pairing,
-{
+impl PartialEq for Signature {
     fn eq(&self, other: &Self) -> bool {
         self.sigma == other.sigma
     }
@@ -290,86 +249,69 @@ where
 
 // TODO insecure!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //
-fn hash_to_curve<P: Pairing>(msg: &[u8]) -> P::G1 {
+fn hash_to_curve(msg: &[u8]) -> G1Projective {
     let hasher_init = &[1u8];
-    let hasher = <DefaultFieldHasher<Sha256> as HashToField<P::ScalarField>>::new(hasher_init);
-    let field_elems: jf_utils::Vec<P::ScalarField> = hasher.hash_to_field(msg, 1);
-    P::G1::generator() * field_elems[0]
+    let hasher = <DefaultFieldHasher<Sha256> as HashToField<ScalarField>>::new(hasher_init);
+    let field_elems: Vec<ScalarField> = hasher.hash_to_field(msg, 1);
+    G1Projective::generator() * field_elems[0]
 }
 
-impl<P> KeyPair<P>
-where
-    P: Pairing,
-{
+impl KeyPair {
     /// Key-pair generation algorithm
-    pub fn generate<R: Rng>(prng: &mut R) -> KeyPair<P> {
+    pub fn generate<R: Rng>(prng: &mut R) -> KeyPair {
         let sk = SignKey::generate(prng);
         let vk = VerKey::from(&sk);
-        KeyPair {
-            phantom: Default::default(),
-            sk,
-            vk,
-        }
+        KeyPair { sk, vk }
     }
 
     /// Key pair generation using a particular sign key secret `sk`
-    pub fn generate_with_sign_key(sk: P::ScalarField) -> Self {
+    pub fn generate_with_sign_key(sk: ScalarField) -> Self {
         let sk = SignKey(sk);
         let vk = VerKey::from(&sk);
-        KeyPair {
-            phantom: Default::default(),
-            sk,
-            vk,
-        }
+        KeyPair { sk, vk }
     }
 
     /// Get reference to verification key
-    pub fn ver_key_ref(&self) -> &VerKey<P> {
+    pub fn ver_key_ref(&self) -> &VerKey {
         &self.vk
     }
 
     /// Get the verification key
-    pub fn ver_key(&self) -> VerKey<P> {
+    pub fn ver_key(&self) -> VerKey {
         self.vk.clone()
     }
 
     /// Get the internal of the signing key, namely a P::ScalarField element
-    pub fn sign_key_internal(&self) -> &P::ScalarField {
+    pub fn sign_key_internal(&self) -> &ScalarField {
         &self.sk.0
     }
 
     /// Signature function
     #[allow(non_snake_case)]
-    pub fn sign<B: AsRef<[u8]>>(&self, msg: &[u8], _csid: B) -> Signature<P> {
+    pub fn sign<B: AsRef<[u8]>>(&self, msg: &[u8], _csid: B) -> Signature {
         // TODO take into account csid
 
-        let hash_value: P::G1 = hash_to_curve::<P>(msg);
+        let hash_value: G1Projective = hash_to_curve(msg);
         let sigma = hash_value * self.sk.0;
         Signature { sigma }
     }
 }
 
-impl<P: Pairing> SignKey<P> {
-    fn generate<R: Rng>(prng: &mut R) -> SignKey<P> {
-        SignKey(P::ScalarField::rand(prng))
+impl SignKey {
+    fn generate<R: Rng>(prng: &mut R) -> SignKey {
+        SignKey(ScalarField::rand(prng))
     }
 }
 
-impl<P> From<&SignKey<P>> for VerKey<P>
-where
-    P: Pairing,
-{
-    fn from(sk: &SignKey<P>) -> Self {
-        VerKey(P::G2::generator() * sk.0)
+impl From<&SignKey> for VerKey {
+    fn from(sk: &SignKey) -> Self {
+        VerKey(G2Projective::generator() * sk.0)
     }
 }
 
-impl<P> VerKey<P>
-where
-    P: Pairing,
-{
+impl VerKey {
     /// Get the internal of verifying key, namely a curve Point
-    pub fn internal(&self) -> P::G2 {
+    pub fn internal(&self) -> G2Projective {
         self.0
     }
 
@@ -378,15 +320,15 @@ where
     pub fn verify<B: AsRef<[u8]>>(
         &self,
         msg: &[u8],
-        sig: &Signature<P>,
+        sig: &Signature,
         _csid: B,
     ) -> Result<(), PrimitivesError> {
         // TODO Check public key
         // TODO take into account csid
 
-        let group_elem = hash_to_curve::<P>(msg);
-        let g2 = P::G2::generator();
-        let is_sig_valid = P::pairing(sig.sigma, g2) == P::pairing(group_elem, self.0);
+        let group_elem = hash_to_curve(msg);
+        let g2 = G2Projective::generator();
+        let is_sig_valid = Bn254::pairing(sig.sigma, g2) == Bn254::pairing(group_elem, self.0);
         if is_sig_valid {
             Ok(())
         } else {
@@ -401,22 +343,22 @@ fn hash_to_curve_bn254(_msg: &[u8]) -> Result<G1Affine, HashToCurveError> {
     struct Bn254CurveConfig;
 
     impl CurveConfig for Bn254CurveConfig {
-        type BaseField = Fq;
-        type ScalarField = Fr;
+        type BaseField = BaseField;
+        type ScalarField = ScalarField;
 
         /// COFACTOR = 1
         const COFACTOR: &'static [u64] = &[0x1];
 
         /// COFACTOR_INV = COFACTOR^{-1} mod r = 1
-        const COFACTOR_INV: Fr = Fr::ONE;
+        const COFACTOR_INV: ScalarField = ScalarField::ONE;
     }
 
     impl SWCurveConfig for Bn254CurveConfig {
         /// COEFF_A = 0
-        const COEFF_A: Fq = Fq::ZERO;
+        const COEFF_A: BaseField = BaseField::ZERO;
 
         /// COEFF_B = 3
-        const COEFF_B: Fq = MontFp!("3");
+        const COEFF_B: BaseField = MontFp!("3");
 
         /// AFFINE_GENERATOR_COEFFS = (G1_GENERATOR_X, G1_GENERATOR_Y)
         const GENERATOR: Affine<Self> = Affine::new_unchecked(G1_GENERATOR_X, G1_GENERATOR_Y);
@@ -435,19 +377,18 @@ fn hash_to_curve_bn254(_msg: &[u8]) -> Result<G1Affine, HashToCurveError> {
         /// to be square. In general we use a `ZETA` with low absolute
         /// value coefficients when they are represented as integers.
 
-        const ZETA: Fq = MontFp!("-1");
+        const ZETA: BaseField = MontFp!("-1");
     }
 
     // TODO hash msg to field element
     let test_map_to_curve = SWUMap::<Bn254CurveConfig>::new().unwrap();
-    let p = test_map_to_curve.map_to_curve(Fq::from(1)).unwrap();
+    let p = test_map_to_curve.map_to_curve(BaseField::from(1)).unwrap();
     Ok(G1Affine::new(p.x, p.y))
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{constants::CS_ID_BLS_MIN_SIG, signatures::bls_arkwors::KeyPair};
-    use ark_bn254::Bn254;
     use ark_ff::vec; // TODO new constant
 
     #[test]
@@ -455,7 +396,7 @@ mod tests {
         // TODO use SignatureScheme instead
 
         let mut rng = jf_utils::test_rng();
-        let key_pair = KeyPair::<Bn254>::generate(&mut rng);
+        let key_pair = KeyPair::generate(&mut rng);
         let msg = vec![15u8, 44u8];
         let sig = key_pair.sign(&msg, CS_ID_BLS_MIN_SIG);
 
