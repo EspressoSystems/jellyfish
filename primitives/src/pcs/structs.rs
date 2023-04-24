3d3dd50fae8359e0ed9f39a4cb321fc3e9092c8f
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-04-24 11:06:45 -0400

    wip: allow generic access to Commitment.0

--- primitives/src/pcs/structs.rs
+++ primitives/src/pcs/structs.rs
@@ -4,7 +4,7 @@
 // You should have received a copy of the MIT License
 // along with the Jellyfish library. If not, see <https://mit-license.org/>.
 
-use ark_ec::pairing::Pairing;
+use ark_ec::{pairing::Pairing, CurveGroup};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use ark_std::vec::Vec;
 
@@ -23,3 +23,36 @@ pub struct Commitment<E: Pairing>(
     /// the actual commitment is an affine point.
     pub E::G1Affine,
 );
+
+/// Allow generic access to the underlying affine point.
+impl<T, E> From<T> for Commitment<E>
+where
+    T: CurveGroup,
+    E: Pairing<G1 = T, G1Affine = T::Affine>,
+{
+    fn from(value: T) -> Self {
+        Self(value.into())
+    }
+}
+
+// https://stackoverflow.com/questions/63119000/why-am-i-required-to-cover-t-in-impl-foreigntraitlocaltype-for-t-e0210
+// https://users.rust-lang.org/t/generic-conversion-from-newtypes/16247
+impl<T, E> From<Commitment<E>> for (T,)
+where
+    T: CurveGroup,
+    E: Pairing<G1 = T, G1Affine = T::Affine>,
+{
+    fn from(value: Commitment<E>) -> Self {
+        (value.0.into(),)
+    }
+}
+
+impl<T, E> From<&Commitment<E>> for (T,)
+where
+    T: CurveGroup,
+    E: Pairing<G1 = T, G1Affine = T::Affine>,
+{
+    fn from(value: &Commitment<E>) -> Self {
+        (value.0.into(),)
+    }
+}
