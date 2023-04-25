7d6b17787e9b3fda85c9167a44fb964ea7e635db
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-04-25 17:54:13 -0400

    impl Error for PCSError

--- primitives/src/pcs/errors.rs
+++ primitives/src/pcs/errors.rs
@@ -28,6 +28,8 @@ pub enum PCSError {
     TranscriptError(TranscriptError),
 }
 
+impl ark_std::error::Error for PCSError {}
+
 impl From<SerializationError> for PCSError {
     fn from(e: ark_serialize::SerializationError) -> Self {
         Self::SerializationError(e)
