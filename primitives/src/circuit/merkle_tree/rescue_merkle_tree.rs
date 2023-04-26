76549997e1bb1897aed37f6c38bb3350046f3775
Michal Swietek <michalswietek88@gmail.com>
2023-04-26 09:04:58 +0200

    Remove leftover file

--- primitives/src/circuit/merkle_tree/rescue_merkle_tree.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022 Espresso Systems (espressosys.com)
-// This file is part of the Jellyfish library.
-
-// You should have received a copy of the MIT License
-// along with the Jellyfish library. If not, see <https://mit-license.org/>.
-
-//! Circuit implementation of an append-only, 3-ary Merkle tree, instantiated
-//! with a Rescue hash function.
