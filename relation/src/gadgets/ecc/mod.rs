12ee629d485d419b98f4740e4c75f9a43ab87c0c
chancharles92 <chancharles92@gmail.com>
2023-05-31 12:37:40 -0400

    make binary_point_select public (#283)

--- relation/src/gadgets/ecc/mod.rs
+++ relation/src/gadgets/ecc/mod.rs
@@ -222,7 +222,7 @@ impl<F: PrimeField> PlonkCircuit<F> {
     /// variables. `b` is a boolean variable that indicates selection of P_b
     /// from (P0, P1).
     /// Return error if invalid input parameters are provided.
-    fn binary_point_vars_select(
+    pub fn binary_point_vars_select(
         &mut self,
         b: BoolVar,
         point0: &PointVariable,
