0103870afd6f8c945a694caac809071ecc96d44c
MRain <linmrain@gmail.com>
2023-05-31 12:00:25 -0400

    emulated field addition

--- relation/src/gadgets/emulated.rs
+++ relation/src/gadgets/emulated.rs
@@ -23,16 +23,20 @@ pub trait EmulationConfig<F: PrimeField>: PrimeField {
     const T: usize;
     /// Bit length of each limbs.
     const B: usize;
-    /// 2^T
-    const T_POW: BigUint;
-    /// 2^B
-    const B_POW: BigUint;
     /// `B * NUM_LIMBS` should equals to `T`.
     const NUM_LIMBS: usize;
-    /// CRT coefficient for native element
-    const CRT_COEF_NATIVE: Self;
-    /// CRT coefficient for power of 2 element
-    const CRT_COEF_POWER_OF_2: Self;
+}
+
+fn biguint_to_limbs<F: PrimeField>(mut val: BigUint, b: usize, num_limbs: usize) -> Vec<F> {
+    let mut result = vec![];
+    let b_pow = BigUint::from(2u32).pow(b as u32);
+
+    // Since q < 2^T, no need to perform mod 2^T
+    for _ in 0..num_limbs {
+        result.push(F::from(&val % &b_pow));
+        val /= &b_pow;
+    }
+    result
 }
 
 /// Convert an element in the emulated field to a list of native field elements.
@@ -41,15 +45,7 @@ where
     E: EmulationConfig<F>,
     F: PrimeField,
 {
-    let q: BigUint = F::MODULUS.into();
-    let val: BigUint = val.into();
-    let mut result = vec![F::from(&val % q)];
-    let mut power_of_2_r = &val % E::T_POW;
-    for _ in 0..E::NUM_LIMBS {
-        result.push(F::from(&power_of_2_r % E::B_POW));
-        power_of_2_r /= E::B_POW;
-    }
-    result
+    biguint_to_limbs(val.into(), E::B, E::NUM_LIMBS)
 }
 
 /// Inverse conversion of the [`from_emulated_field`]
@@ -58,16 +54,17 @@ where
     E: EmulationConfig<F>,
     F: PrimeField,
 {
-    if vals.len() != E::NUM_LIMBS + 1 {
+    if vals.len() != E::NUM_LIMBS {
         return Err(CircuitError::FieldAlgebraError(
             "Malformed structure for emulated field element conversion.".to_string(),
         ));
     }
-    let native = E::from(<F as Into<BigUint>>::into(vals[0]));
-    let power_of_2_r = E::from(vals.iter().skip(1).rfold(BigUint::zero(), |result, &val| {
-        result * E::B_POW + <F as Into<BigUint>>::into(val)
-    }));
-    Ok(native * E::CRT_COEF_NATIVE + power_of_2_r * E::CRT_COEF_POWER_OF_2)
+    let b_pow = BigUint::from(2u32).pow(E::B as u32);
+    Ok(E::from(
+        vals.iter().rfold(BigUint::zero(), |result, &val| {
+            result * &b_pow + <F as Into<BigUint>>::into(val)
+        }),
+    ))
 }
 
 /// The variable represents an element in the emulated field.
@@ -174,25 +171,55 @@ impl<F: PrimeField> PlonkCircuit<F> {
     }
 
     /// Constrain that a+b=c in the emulated field.
+    /// Checking whether a + b = k * E::MODULUS + c
     pub fn emulated_add_gate<E: EmulationConfig<F>>(
         &mut self,
         a: &EmulatedVariable<E>,
         b: &EmulatedVariable<E>,
         c: &EmulatedVariable<E>,
     ) -> Result<(), CircuitError> {
-        self.add_gate(a.0[0], b.0[0], c.0[0])?;
+        let val_a: BigUint = self.emulated_witness(a)?.into();
+        let val_b: BigUint = self.emulated_witness(b)?.into();
+        let q: BigUint = E::MODULUS.into();
+        let b_pow = BigUint::from(2u32).pow(E::B as u32);
+        let add_no_mod = &val_a + &val_b;
+        let k = if add_no_mod > q { 1u32 } else { 0u32 };
+        let var_k = self.create_boolean_variable(add_no_mod > q)?.0;
+        let q_limbs = biguint_to_limbs::<F>(q, E::B, E::NUM_LIMBS);
+
+        let add_no_mod_limbs = biguint_to_limbs::<F>(add_no_mod, E::B, E::NUM_LIMBS)
+            .into_iter()
+            .map(|val| self.create_variable(val))
+            .collect::<Result<Vec<_>, CircuitError>>()?;
+
+        // Checking whether a + b = add_no_mod_limbs
         let mut carry_out = self.zero();
-        for (a, b, c) in izip!(&a.0, &b.0, &c.0).skip(1) {
+        for (a, b, c) in izip!(&a.0, &b.0, &add_no_mod_limbs) {
             let next_carry_out =
-                F::from((self.witness(*a)? + self.witness(*b)?).into_bigint().into() % E::B_POW);
+                F::from(<F as Into<BigUint>>::into(self.witness(*a)? + self.witness(*b)?) / &b_pow);
             let next_carry_out = self.create_variable(next_carry_out)?;
 
             let wires = [*a, *b, carry_out, next_carry_out, *c];
-            let coeffs = [F::one(), F::one(), F::one(), -F::from(E::B_POW)];
+            let coeffs = [F::one(), F::one(), F::one(), -F::from(b_pow.clone())];
             self.lc_gate(&wires, &coeffs)?;
             carry_out = next_carry_out;
 
             self.enforce_in_range(*c, E::B)?;
+        }
+
+        // Checking whether k * q + c = add_no_mod_limbs
+        carry_out = self.zero();
+        for (a, b, c) in izip!(q_limbs, &c.0, &add_no_mod_limbs) {
+            let next_carry_out =
+                F::from(<F as Into<BigUint>>::into(a * F::from(k) + self.witness(*b)?) / &b_pow);
+            let next_carry_out = self.create_variable(next_carry_out)?;
+
+            let wires = [var_k, *b, carry_out, next_carry_out, *c];
+            let coeffs = [a, F::one(), F::one(), -F::from(b_pow.clone())];
+            self.lc_gate(&wires, &coeffs)?;
+            carry_out = next_carry_out;
+
+            self.enforce_in_range(*b, E::B)?;
         }
         Ok(())
     }
@@ -216,20 +243,49 @@ impl<F: PrimeField> PlonkCircuit<F> {
         b: E,
         c: &EmulatedVariable<E>,
     ) -> Result<(), CircuitError> {
-        let b = from_emulated_field(b);
-        self.add_constant_gate(a.0[0], b[0], c.0[0])?;
+        let val_a: BigUint = self.emulated_witness(a)?.into();
+        let val_b: BigUint = b.into();
+        let q: BigUint = E::MODULUS.into();
+        let b_pow = BigUint::from(2u32).pow(E::B as u32);
+        let add_no_mod = &val_a + &val_b;
+        let k = if add_no_mod > q { 1u32 } else { 0u32 };
+        let var_k = self.create_boolean_variable(add_no_mod > q)?.0;
+        let q_limbs = biguint_to_limbs::<F>(q, E::B, E::NUM_LIMBS);
+        let b_limbs = biguint_to_limbs::<F>(val_b, E::B, E::NUM_LIMBS);
 
+        let add_no_mod_limbs = biguint_to_limbs::<F>(add_no_mod, E::B, E::NUM_LIMBS)
+            .into_iter()
+            .map(|val| self.create_variable(val))
+            .collect::<Result<Vec<_>, CircuitError>>()?;
+
+        // Checking whether a + b = add_no_mod_limbs
         let mut carry_out = self.zero();
-        for (a, b, c) in izip!(&a.0, b, &c.0).skip(1) {
-            let next_carry_out = F::from((self.witness(*a)? + b).into_bigint().into() % E::B_POW);
+        for (a, b, c) in izip!(&a.0, b_limbs, &add_no_mod_limbs) {
+            let next_carry_out =
+                F::from(<F as Into<BigUint>>::into(self.witness(*a)? + b) / &b_pow);
             let next_carry_out = self.create_variable(next_carry_out)?;
 
             let wires = [*a, self.one(), carry_out, next_carry_out, *c];
-            let coeffs = [F::one(), b, F::one(), -F::from(E::B_POW)];
+            let coeffs = [F::one(), b, F::one(), -F::from(b_pow.clone())];
             self.lc_gate(&wires, &coeffs)?;
             carry_out = next_carry_out;
 
             self.enforce_in_range(*c, E::B)?;
+        }
+
+        // Checking whether k * q + c = add_no_mod_limbs
+        carry_out = self.zero();
+        for (a, b, c) in izip!(q_limbs, &c.0, &add_no_mod_limbs) {
+            let next_carry_out =
+                F::from(<F as Into<BigUint>>::into(a * F::from(k) + self.witness(*b)?) / &b_pow);
+            let next_carry_out = self.create_variable(next_carry_out)?;
+
+            let wires = [var_k, *b, carry_out, next_carry_out, *c];
+            let coeffs = [a, F::one(), F::one(), -F::from(b_pow.clone())];
+            self.lc_gate(&wires, &coeffs)?;
+            carry_out = next_carry_out;
+
+            self.enforce_in_range(*b, E::B)?;
         }
         Ok(())
     }
@@ -245,6 +301,14 @@ impl<F: PrimeField> PlonkCircuit<F> {
         self.emulated_add_constant_gate(a, b, &c)?;
         Ok(c)
     }
+}
+
+impl EmulationConfig<ark_bn254::Fr> for ark_bls12_377::Fq {
+    const T: usize = 500;
+
+    const B: usize = 125;
+
+    const NUM_LIMBS: usize = 4;
 }
 
 #[cfg(test)]
