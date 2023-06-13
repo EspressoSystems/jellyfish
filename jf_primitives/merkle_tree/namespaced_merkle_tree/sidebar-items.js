0ed0385907f2e134be36cea4dc7479c908c31825
nomaxg <nomaxg@users.noreply.github.com>
2023-06-13 14:53:22 +0000

    deploy: 4e28dd02a0ab6fde0887713bc65b58f4dbe8cd60

--- jf_primitives/merkle_tree/namespaced_merkle_tree/sidebar-items.js
+++ jf_primitives/merkle_tree/namespaced_merkle_tree/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["NamespacedHash","NamespacedHasher"],"trait":["BindNamespace","Namespace","Namespaced","NamespacedMerkleTreeScheme"]};
+window.SIDEBAR_ITEMS = {"struct":["NMT","NaiveNamespaceProof","NamespacedHash","NamespacedHasher"],"trait":["BindNamespace","Namespace","NamespaceProof","Namespaced","NamespacedMerkleTreeScheme"]};
