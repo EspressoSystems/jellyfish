26ae2f2b62304f774771b3adcec703ad7f6f6095
MRain <linmrain@gmail.com>
2023-06-22 15:23:10 -0400

    rename symbols for readability

--- relation/src/gadgets/ecc/emulated/short_weierstrass.rs
+++ relation/src/gadgets/ecc/emulated/short_weierstrass.rs
@@ -6,10 +6,9 @@
 
 //! Short Weierstrass curve point addition
 
-use super::EmulatedTEPointVariable;
 use crate::{
     errors::CircuitError,
-    gadgets::{ecc::TEPoint, EmulatedVariable, EmulationConfig},
+    gadgets::{EmulatedVariable, EmulationConfig},
     BoolVar, Circuit, PlonkCircuit,
 };
 use ark_ec::short_weierstrass::{Affine, SWCurveConfig};
@@ -137,83 +136,91 @@ impl<F: PrimeField> PlonkCircuit<F> {
 
     /// Constrain variable `c` to be the point addition of `a` and
     /// `b` over an elliptic curve.
-    /// Let a = (x1, y1), b = (x2, y2), c = (x3, y3)
+    /// Let a = (x0, y0), b = (x1, y1), c = (x2, y2)
     /// The addition formula for affine points of sw curve is
     ///   1. if a == b
-    ///     - if y1 == 0 then (x3, y3) = (0, 0)
-    ///     - Calculate s = (3 * x1^2 + d) / (2 * y1)
-    ///     - x3 = s^2 - 2 * x1
-    ///     - y3 = s(x1 - x3) - y1
+    ///     - if y0 == 0 then (x2, y2) = (0, 0)
+    ///     - Calculate s = (3 * x0^2 + a) / (2 * y0)
+    ///     - x2 = s^2 - 2 * x0
+    ///     - y2 = s(x0 - x2) - y0
     ///   2. Otherwise
-    ///     - if x1 == x2 then (x3, y3) = (0, 0)
-    ///     - Calculate s = (y1 - y2) / (x1 - x2)
-    ///     - x3 = s^2 - x1 - x2
-    ///     - y3 = s(x1 - x3) - y1
+    ///     - if x0 == x1 then (x2, y2) = (0, 0)
+    ///     - Calculate s = (y0 - y1) / (x0 - x1)
+    ///     - x2 = s^2 - x0 - x1
+    ///     - y2 = s(x0 - x2) - y0
     /// The first case is equivalent to the following:
-    ///   - (x3 + 2 * x1) * (y1 + y1) * y1 = (3* x_1^2 + d)^2 * y1
-    ///   - (y3 + y1) * 2 * y1 * y1 = (3* x_1^2 + d) (x1 - x3) * y1
-    ///   - not_equal(x1, x2) || not_equal(y1, y2) || not_zero(y1) || ((x3 == 0)
-    ///     && (y3 == 0))
+    ///   - (x2 + 2 * x0) * (y0 + y0) * y0 = (3* x_1^2 + d)^2 * y0
+    ///   - (y2 + y0) * 2 * y0 * y0 = (3* x_1^2 + d) (x0 - x2) * y0
+    ///   - not_equal(x0, x1) || not_equal(y0, y1) || not_zero(y0) || ((x2 == 0)
+    ///     && (y2 == 0))
     /// The second case is equivalent to the following:
-    ///   - (x1 - x2)^3 (x1 + x2 + x3) == (x1 - x2) (y1 - y2)^2
-    ///   - (x1 - x2) (x1 - x3) (y1 - y2) == (y1 + y3) (x1 - x2)^2
-    ///   - not_equal(x1, x2) || is_equal(y1, y2) || ((x3 == 0) && (y3 == 0))
+    ///   - (x0 - x1)^3 (x0 + x1 + x2) == (x0 - x1) (y0 - y1)^2
+    ///   - (x0 - x1) (x0 - x2) (y0 - y1) == (y0 + y2) (x0 - x1)^2
+    ///   - not_equal(x0, x1) || is_equal(y0, y1) || ((x2 == 0) && (y2 == 0))
     /// TODO: unfinished
     pub fn emulated_sw_ecc_add_gate<E: EmulationConfig<F>>(
         &mut self,
-        _a: &EmulatedTEPointVariable<E>,
-        _b: &EmulatedTEPointVariable<E>,
-        _c: &EmulatedTEPointVariable<E>,
-        _d: E,
+        p0: &EmulatedSWPointVariable<E>,
+        p1: &EmulatedSWPointVariable<E>,
+        p2: &EmulatedSWPointVariable<E>,
+        _a: E,
     ) -> Result<(), CircuitError> {
+        let eq_p1_p2 = self.is_emulated_sw_point_equal(p1, p2)?;
+        let eq_p0_p2 = self.is_emulated_sw_point_equal(p0, p2)?;
+        // Case 1: either p0 or p1 is infinity
+        self.enforce_equal(p0.2 .0, eq_p1_p2.0)?;
+        self.enforce_equal(p1.2 .0, eq_p0_p2.0)?;
         todo!()
     }
 
-    /// Obtain a variable to the point addition result of `a` + `b`
+    /// Obtain a variable to the point addition result of `p0` + `p1`
     pub fn emulated_sw_ecc_add<E: EmulationConfig<F>>(
         &mut self,
-        a: &EmulatedTEPointVariable<E>,
-        b: &EmulatedTEPointVariable<E>,
-        d: E,
-    ) -> Result<EmulatedTEPointVariable<E>, CircuitError> {
-        let x1 = self.emulated_witness(&a.0)?;
-        let y1 = self.emulated_witness(&a.1)?;
-        let x2 = self.emulated_witness(&b.0)?;
-        let y2 = self.emulated_witness(&b.1)?;
-        let (x3, y3) = if x1 == x2 && y1 == y2 {
+        p0: &EmulatedSWPointVariable<E>,
+        p1: &EmulatedSWPointVariable<E>,
+        a: E,
+    ) -> Result<EmulatedSWPointVariable<E>, CircuitError> {
+        let x0 = self.emulated_witness(&p0.0)?;
+        let y0 = self.emulated_witness(&p0.1)?;
+        let infinity0 = self.witness(p0.2 .0)? == F::one();
+        let x1 = self.emulated_witness(&p1.0)?;
+        let y1 = self.emulated_witness(&p1.1)?;
+        let infinity1 = self.witness(p1.2 .0)? == F::one();
+        let p2 = if infinity0 {
+            SWPoint(x1, y1, infinity1)
+        } else if infinity1 {
+            SWPoint(x0, y0, infinity0)
+        } else if x0 == x1 && y0 == y1 {
             // point doubling
-            if y1.is_zero() {
-                (E::zero(), E::zero())
+            if y0.is_zero() {
+                SWPoint(E::zero(), E::zero(), true)
             } else {
-                let s = (x1 * x1 * E::from(3u64) + d) / (y1 + y1);
-                let x3 = s * s - x1 - x2;
-                let y3 = s * (x1 - x3) - y1;
-                (x3, y3)
+                let s = (x0 * x0 * E::from(3u64) + a) / (y0 + y0);
+                let x2 = s * s - x0 - x1;
+                let y2 = s * (x0 - x2) - y0;
+                SWPoint(x2, y2, false)
             }
         } else {
             // point addition
-            if x1 == x2 {
-                (E::zero(), E::zero())
+            if x0 == x1 {
+                SWPoint(E::zero(), E::zero(), true)
             } else {
-                let s = (y1 - y2) / (x1 - x2);
-                let x3 = s * s - x1 - x2;
-                let y3 = s * (x1 - x3) - y1;
-                (x3, y3)
+                let s = (y0 - y1) / (x0 - x1);
+                let x2 = s * s - x0 - x1;
+                let y2 = s * (x0 - x2) - y0;
+                SWPoint(x2, y2, false)
             }
         };
-        let c = self.create_emulated_te_point_variable(TEPoint(x3, y3))?;
-        // self.emulated_sw_ecc_add_gate(a, b, &c, d)?;
-        Ok(c)
+        let p2 = self.create_emulated_sw_point_variable(p2)?;
+        self.emulated_sw_ecc_add_gate(p0, p1, &p2, a)?;
+        Ok(p2)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
-        gadgets::{
-            ecc::{conversion::*, TEPoint},
-            EmulationConfig,
-        },
+        gadgets::{ecc::conversion::*, EmulationConfig},
         Circuit, PlonkCircuit,
     };
     use ark_bls12_377::{g1::Config as Param377, Fq as Fq377};
@@ -233,7 +240,7 @@ mod tests {
         test_emulated_sw_point_addition_helper::<Fq254, Fr254, Param254>(a);
     }
 
-    fn test_emulated_sw_point_addition_helper<E, F, P>(d: E)
+    fn test_emulated_sw_point_addition_helper<E, F, P>(a: E)
     where
         E: EmulationConfig<F> + SWToTEConParam,
         F: PrimeField,
@@ -243,44 +250,44 @@ mod tests {
         let neutral = Projective::<P>::zero().into_affine();
         let p1 = Projective::<P>::rand(&mut rng).into_affine();
         let p2 = Projective::<P>::rand(&mut rng).into_affine();
-        let expected: TEPoint<E> = (p1 + p2).into_affine().into();
-        let wrong_result: TEPoint<E> = (p1 + p2 + Projective::<P>::generator())
+        let expected = (p1 + p2).into_affine().into();
+        let wrong_result = (p1 + p2 + Projective::<P>::generator())
             .into_affine()
             .into();
 
         let mut circuit = PlonkCircuit::<F>::new_turbo_plonk();
 
         let var_p1 = circuit
-            .create_emulated_te_point_variable(p1.into())
+            .create_emulated_sw_point_variable(p1.into())
             .unwrap();
         let var_p2 = circuit
-            .create_emulated_te_point_variable(p2.into())
+            .create_emulated_sw_point_variable(p2.into())
             .unwrap();
-        let var_result = circuit.emulated_sw_ecc_add(&var_p1, &var_p2, d).unwrap();
+        let var_result = circuit.emulated_sw_ecc_add(&var_p1, &var_p2, a).unwrap();
         assert_eq!(
-            circuit.emulated_te_point_witness(&var_result).unwrap(),
+            circuit.emulated_sw_point_witness(&var_result).unwrap(),
             expected
         );
         let var_neutral = circuit
-            .create_emulated_te_point_variable(neutral.into())
+            .create_emulated_sw_point_variable(neutral.into())
             .unwrap();
         let var_neutral_result1 = circuit
-            .emulated_sw_ecc_add(&var_p1, &var_neutral, d)
+            .emulated_sw_ecc_add(&var_p1, &var_neutral, a)
             .unwrap();
         let var_neutral_result2 = circuit
-            .emulated_sw_ecc_add(&var_neutral, &var_p1, d)
+            .emulated_sw_ecc_add(&var_neutral, &var_p1, a)
             .unwrap();
         assert_eq!(
             circuit
-                .emulated_te_point_witness(&var_neutral_result1)
+                .emulated_sw_point_witness(&var_neutral_result1)
                 .unwrap(),
             p1.into()
         );
         assert_eq!(
             circuit
-                .emulated_te_point_witness(&var_neutral_result2)
+                .emulated_sw_point_witness(&var_neutral_result2)
                 .unwrap(),
-            p2.into()
+            p1.into()
         );
         assert!(circuit.check_circuit_satisfiability(&[]).is_ok());
 
@@ -288,10 +295,10 @@ mod tests {
 
         // fail path
         let var_wrong_result = circuit
-            .create_emulated_te_point_variable(wrong_result)
+            .create_emulated_sw_point_variable(wrong_result)
             .unwrap();
         circuit
-            .emulated_sw_ecc_add_gate(&var_p1, &var_p2, &var_wrong_result, d)
+            .emulated_sw_ecc_add_gate(&var_p1, &var_p2, &var_wrong_result, a)
             .unwrap();
         assert!(circuit.check_circuit_satisfiability(&[]).is_err());
     }
--- relation/src/gadgets/ecc/emulated/twisted_edwards.rs
+++ relation/src/gadgets/ecc/emulated/twisted_edwards.rs
@@ -31,30 +31,30 @@ impl<F: PrimeField> PlonkCircuit<F> {
     /// Add a new emulated EC point (as witness)
     pub fn create_emulated_te_point_variable<E: EmulationConfig<F>>(
         &mut self,
-        point: TEPoint<E>,
+        p: TEPoint<E>,
     ) -> Result<EmulatedTEPointVariable<E>, CircuitError> {
-        let x = self.create_emulated_variable(point.0)?;
-        let y = self.create_emulated_variable(point.1)?;
+        let x = self.create_emulated_variable(p.0)?;
+        let y = self.create_emulated_variable(p.1)?;
         Ok(EmulatedTEPointVariable(x, y))
     }
 
     /// Add a new constant emulated EC point
     pub fn create_constant_emulated_te_point_variable<E: EmulationConfig<F>>(
         &mut self,
-        point: TEPoint<E>,
+        p: TEPoint<E>,
     ) -> Result<EmulatedTEPointVariable<E>, CircuitError> {
-        let x = self.create_constant_emulated_variable(point.0)?;
-        let y = self.create_constant_emulated_variable(point.1)?;
+        let x = self.create_constant_emulated_variable(p.0)?;
+        let y = self.create_constant_emulated_variable(p.1)?;
         Ok(EmulatedTEPointVariable(x, y))
     }
 
     /// Add a new public emulated EC point
     pub fn create_public_emulated_te_point_variable<E: EmulationConfig<F>>(
         &mut self,
-        point: TEPoint<E>,
+        p: TEPoint<E>,
     ) -> Result<EmulatedTEPointVariable<E>, CircuitError> {
-        let x = self.create_public_emulated_variable(point.0)?;
-        let y = self.create_public_emulated_variable(point.1)?;
+        let x = self.create_public_emulated_variable(p.0)?;
+        let y = self.create_public_emulated_variable(p.1)?;
         Ok(EmulatedTEPointVariable(x, y))
     }
 
@@ -65,11 +65,11 @@ impl<F: PrimeField> PlonkCircuit<F> {
     pub fn binary_emulated_te_point_vars_select<E: EmulationConfig<F>>(
         &mut self,
         b: BoolVar,
-        point0: &EmulatedTEPointVariable<E>,
-        point1: &EmulatedTEPointVariable<E>,
+        p0: &EmulatedTEPointVariable<E>,
+        p1: &EmulatedTEPointVariable<E>,
     ) -> Result<EmulatedTEPointVariable<E>, CircuitError> {
-        let select_x = self.conditional_select_emulated(b, &point0.0, &point1.0)?;
-        let select_y = self.conditional_select_emulated(b, &point0.1, &point1.1)?;
+        let select_x = self.conditional_select_emulated(b, &p0.0, &p1.0)?;
+        let select_y = self.conditional_select_emulated(b, &p0.1, &p1.1)?;
 
         Ok(EmulatedTEPointVariable::<E>(select_x, select_y))
     }
@@ -78,11 +78,11 @@ impl<F: PrimeField> PlonkCircuit<F> {
     /// Return error if the input point variables are invalid.
     pub fn enforce_emulated_te_point_equal<E: EmulationConfig<F>>(
         &mut self,
-        point0: &EmulatedTEPointVariable<E>,
-        point1: &EmulatedTEPointVariable<E>,
+        p0: &EmulatedTEPointVariable<E>,
+        p1: &EmulatedTEPointVariable<E>,
     ) -> Result<(), CircuitError> {
-        self.enforce_emulated_var_equal(&point0.0, &point1.0)?;
-        self.enforce_emulated_var_equal(&point0.1, &point1.1)?;
+        self.enforce_emulated_var_equal(&p0.0, &p1.0)?;
+        self.enforce_emulated_var_equal(&p0.1, &p1.1)?;
         Ok(())
     }
 
@@ -90,66 +90,66 @@ impl<F: PrimeField> PlonkCircuit<F> {
     /// variables are equal. Return error if variables are invalid.
     pub fn is_emulated_te_point_equal<E: EmulationConfig<F>>(
         &mut self,
-        point0: &EmulatedTEPointVariable<E>,
-        point1: &EmulatedTEPointVariable<E>,
+        p0: &EmulatedTEPointVariable<E>,
+        p1: &EmulatedTEPointVariable<E>,
     ) -> Result<BoolVar, CircuitError> {
-        let mut r0 = self.is_emulated_var_equal(&point0.0, &point1.0)?;
-        let r1 = self.is_emulated_var_equal(&point0.1, &point1.1)?;
+        let mut r0 = self.is_emulated_var_equal(&p0.0, &p1.0)?;
+        let r1 = self.is_emulated_var_equal(&p0.1, &p1.1)?;
         r0.0 = self.mul(r0.0, r1.0)?;
         Ok(r0)
     }
 
-    /// Constrain variable `c` to be the point addition of `a` and
-    /// `b` over an elliptic curve.
+    /// Constrain variable `p2` to be the point addition of `p0` and
+    /// `p1` over an elliptic curve.
     pub fn emulated_te_ecc_add_gate<E: EmulationConfig<F>>(
         &mut self,
-        a: &EmulatedTEPointVariable<E>,
-        b: &EmulatedTEPointVariable<E>,
-        c: &EmulatedTEPointVariable<E>,
+        p0: &EmulatedTEPointVariable<E>,
+        p1: &EmulatedTEPointVariable<E>,
+        p2: &EmulatedTEPointVariable<E>,
         d: E,
     ) -> Result<(), CircuitError> {
-        let x1y2 = self.emulated_mul(&a.0, &b.1)?;
-        let x2y1 = self.emulated_mul(&b.0, &a.1)?;
-        let x1x2 = self.emulated_mul(&a.0, &b.0)?;
-        let y1y2 = self.emulated_mul(&a.1, &b.1)?;
-        let x1x2y1y2 = self.emulated_mul(&x1x2, &y1y2)?;
-        let dx1x2y1y2 = self.emulated_mul_constant(&x1x2y1y2, d)?;
+        let x0y1 = self.emulated_mul(&p0.0, &p1.1)?;
+        let x1y0 = self.emulated_mul(&p1.0, &p0.1)?;
+        let x0x1 = self.emulated_mul(&p0.0, &p1.0)?;
+        let y0y1 = self.emulated_mul(&p0.1, &p1.1)?;
+        let x0x1y0y1 = self.emulated_mul(&x0x1, &y0y1)?;
+        let dx0x1y0y1 = self.emulated_mul_constant(&x0x1y0y1, d)?;
 
-        // checking that x3 = x1y2 + x2y1 - dx1y1x2y2x3
-        // t1 = x1y2 + x2y1
-        let t1 = self.emulated_add(&x1y2, &x2y1)?;
-        // t2 = d x1 y1 x2 y2 x3
-        let t2 = self.emulated_mul(&dx1x2y1y2, &c.0)?;
-        self.emulated_add_gate(&c.0, &t2, &t1)?;
+        // checking that x2 = x0y1 + x1y0 - dx0y0x1y1x2
+        // t1 = x0y1 + x1y0
+        let t1 = self.emulated_add(&x0y1, &x1y0)?;
+        // t2 = d x0 y0 x1 y1 x2
+        let t2 = self.emulated_mul(&dx0x1y0y1, &p2.0)?;
+        self.emulated_add_gate(&p2.0, &t2, &t1)?;
 
-        // checking that y3 = x1x2 + y1y2 + dx1y1x2y2y3
-        // t1 = x1x2 + y1y2
-        let t1 = self.emulated_add(&x1x2, &y1y2)?;
-        let t2 = self.emulated_mul(&dx1x2y1y2, &c.1)?;
-        self.emulated_add_gate(&t1, &t2, &c.1)
+        // checking that y2 = x0x1 + y0y1 + dx0y0x1y1y2
+        // t1 = x0x1 + y0y1
+        let t1 = self.emulated_add(&x0x1, &y0y1)?;
+        let t2 = self.emulated_mul(&dx0x1y0y1, &p2.1)?;
+        self.emulated_add_gate(&t1, &t2, &p2.1)
     }
 
     /// Obtain a variable to the point addition result of `a` + `b`
     pub fn emulated_te_ecc_add<E: EmulationConfig<F>>(
         &mut self,
-        a: &EmulatedTEPointVariable<E>,
-        b: &EmulatedTEPointVariable<E>,
+        p0: &EmulatedTEPointVariable<E>,
+        p1: &EmulatedTEPointVariable<E>,
         d: E,
     ) -> Result<EmulatedTEPointVariable<E>, CircuitError> {
-        let x1 = self.emulated_witness(&a.0)?;
-        let y1 = self.emulated_witness(&a.1)?;
-        let x2 = self.emulated_witness(&b.0)?;
-        let y2 = self.emulated_witness(&b.1)?;
+        let x0 = self.emulated_witness(&p0.0)?;
+        let y0 = self.emulated_witness(&p0.1)?;
+        let x1 = self.emulated_witness(&p1.0)?;
+        let y1 = self.emulated_witness(&p1.1)?;
 
-        let t1 = x1 * y2;
-        let t2 = x2 * y1;
-        let dx1x2y1y2 = d * t1 * t2;
+        let t1 = x0 * y1;
+        let t2 = x1 * y0;
+        let dx0x1y0y1 = d * t1 * t2;
 
-        let x3 = (t1 + t2) / (E::one() + dx1x2y1y2);
-        let y3 = (x1 * x2 + y1 * y2) / (E::one() - dx1x2y1y2);
-        let c = self.create_emulated_te_point_variable(TEPoint(x3, y3))?;
-        self.emulated_te_ecc_add_gate(a, b, &c, d)?;
-        Ok(c)
+        let x2 = (t1 + t2) / (E::one() + dx0x1y0y1);
+        let y2 = (x0 * x1 + y0 * y1) / (E::one() - dx0x1y0y1);
+        let p2 = self.create_emulated_te_point_variable(TEPoint(x2, y2))?;
+        self.emulated_te_ecc_add_gate(p0, p1, &p2, d)?;
+        Ok(p2)
     }
 }
 
