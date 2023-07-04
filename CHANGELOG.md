2ed912403eddfc9b925b520f466ea8253131848e
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-07-04 12:19:42 -0400

    feat(vid): port VID from another repo (#337)
    * port vid
    
    * update CHANGELOG
    
    * cargo +nightly fmt
    
    * fix no_std support
    
    * nothing to see here, move along
    
    * ci: enable test-srs feature in jf-primitives test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,6 +30,7 @@ and follow [semantic versioning](https://semver.org/) for our releases.
 - [#291](https://github.com/EspressoSystems/jellyfish/pull/291) Non-native field operations and elliptic curve addition
 - [#309](https://github.com/EspressoSystems/jellyfish/pull/309) Reed-Solomon decoder accept FFT domain
 - [#320](https://github.com/EspressoSystems/jellyfish/pull/320) Non-native elliptic curve addition in short Weierstrass form
+- [#337](https://github.com/EspressoSystems/jellyfish/pull/337) Port VID from another repo
 
 ### Changed
 
---
+++ primitives/tests/advz.rs
@@ -0,0 +1,41 @@
+#![cfg(feature = "test-srs")]
+use ark_bls12_381::Bls12_381;
+use ark_ff::{Field, PrimeField};
+use jf_primitives::{
+    pcs::{checked_fft_size, prelude::UnivariateKzgPCS, PolynomialCommitmentScheme},
+    vid::advz::Advz,
+};
+use sha2::Sha256;
+
+mod vid;
+
+#[test]
+fn round_trip() {
+    // play with these items
+    let vid_sizes = [(2, 3), (5, 9)];
+    let byte_lens = [2, 16, 32, 47, 48, 49, 64, 100, 400];
+
+    // more items as a function of the above
+    let supported_degree = vid_sizes.iter().max_by_key(|v| v.0).unwrap().0;
+    let mut rng = jf_utils::test_rng();
+    let srs = UnivariateKzgPCS::<Bls12_381>::gen_srs_for_testing(
+        &mut rng,
+        checked_fft_size(supported_degree).unwrap(),
+    )
+    .unwrap();
+
+    println!(
+            "modulus byte len: {}",
+            (<<UnivariateKzgPCS<Bls12_381> as PolynomialCommitmentScheme>::Evaluation as Field>::BasePrimeField
+                ::MODULUS_BIT_SIZE - 7)/8 + 1
+        );
+
+    vid::round_trip(
+        |payload_chunk_size, num_storage_nodes| {
+            Advz::<Bls12_381, Sha256>::new(payload_chunk_size, num_storage_nodes, &srs).unwrap()
+        },
+        &vid_sizes,
+        &byte_lens,
+        &mut rng,
+    );
+}
