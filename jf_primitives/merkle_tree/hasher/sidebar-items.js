3427bd5b64d222a991205ffca74acb428c0fca43
ggutoski <ggutoski@users.noreply.github.com>
2023-07-05 16:34:12 +0000

    deploy: f2970801da5b356e790de595b5b81557df7fd189

--- jf_primitives/merkle_tree/hasher/sidebar-items.js
+++ jf_primitives/merkle_tree/hasher/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["HasherDigestAlgorithm","HasherNode"],"type":["GenericHasherMerkleTree","HasherMerkleTree"]};
+window.SIDEBAR_ITEMS = {"struct":["HasherDigestAlgorithm","HasherNode"],"trait":["HasherDigest"],"type":["GenericHasherMerkleTree","HasherMerkleTree"]};
