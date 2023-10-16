e08158e5afec6b5ecfb9351718b19af41d70860e
tessico <tessico@protonmail.com>
2023-10-16 23:06:45 +0200

    Use criterion for consistent benches

--- primitives/benches/pcs.rs
+++ primitives/benches/pcs.rs
@@ -4,100 +4,121 @@
 // You should have received a copy of the MIT License
 // along with the Jellyfish library. If not, see <https://mit-license.org/>.
 
-#[cfg(not(feature = "test-srs"))]
-fn main() {
-    panic!("We need features=['bench'] to run this benchmark.");
+#![cfg(feature = "test-srs")]
+
+use ark_bn254::{Bn254, Fr};
+use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
+use ark_std::UniformRand;
+use criterion::{criterion_group, criterion_main, Criterion};
+use jf_primitives::pcs::prelude::{MultilinearKzgPCS, PolynomialCommitmentScheme, MLE};
+use jf_primitives::pcs::StructuredReferenceString;
+use jf_utils::test_rng;
+
+const SAMPLES: usize = 100;
+
+fn commit(c: &mut Criterion) {
+    let max_num_vars = 18;
+    let nv = 17;
+
+    let mut rng = &mut test_rng();
+    let uni_params =
+        MultilinearKzgPCS::<Bn254>::gen_srs_for_testing(&mut rng, max_num_vars).unwrap();
+
+    let (ml_ck, _ml_vk) = uni_params.0.trim(nv).unwrap();
+    let (uni_ck, _uni_vk) = uni_params.1.trim(nv).unwrap();
+    let ck = (ml_ck, uni_ck);
+
+    let polys = (0..SAMPLES)
+        .map(|_| MLE::from(DenseMultilinearExtension::rand(nv, &mut rng)))
+        .collect::<Vec<_>>();
+
+    // this is a little ugly, but ideally we want to avoid cloning inside the benchmark. Therefore we keep `labeled_polys` in scope, and just commit to references to it.
+
+    c.bench_function("Multilinear KZG Commit", |b| {
+        let mut i = 0;
+        b.iter(|| {
+            i = (i + 1) % SAMPLES;
+            let _commit = MultilinearKzgPCS::commit(&ck, &polys[i]).unwrap();
+        })
+    });
 }
 
-#[cfg(feature = "test-srs")]
-fn main() -> Result<(), jf_primitives::pcs::prelude::PCSError> {
-    bench::bench_pcs()
+fn open(c: &mut Criterion) {
+    let max_num_vars = 18;
+    let nv = 17;
+
+    let mut rng = &mut test_rng();
+    let uni_params =
+        MultilinearKzgPCS::<Bn254>::gen_srs_for_testing(&mut rng, max_num_vars).unwrap();
+
+    let (ml_ck, _ml_vk) = uni_params.0.trim(nv).unwrap();
+    let (uni_ck, _uni_vk) = uni_params.1.trim(nv).unwrap();
+    let ck = (ml_ck, uni_ck);
+
+    let polys = (0..SAMPLES)
+        .map(|_| MLE::from(DenseMultilinearExtension::rand(nv, &mut rng)))
+        .collect::<Vec<_>>();
+
+    let points: Vec<_> = (0..SAMPLES)
+        .map(|_| (0..nv).map(|_| Fr::rand(&mut rng)).collect())
+        .collect();
+
+    c.bench_function("Multilinear KZG Open", |b| {
+        let mut i = 0;
+        b.iter(|| {
+            i = (i + 1) % SAMPLES;
+            let _open = MultilinearKzgPCS::open(&ck, &polys[i], &points[i]).unwrap();
+        })
+    });
 }
 
-#[cfg(feature = "test-srs")]
-mod bench {
-    use ark_bls12_381::{Bls12_381, Fr};
-    use ark_ff::UniformRand;
-    use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
-    use jf_primitives::pcs::{
-        prelude::{MultilinearKzgPCS, PCSError, PolynomialCommitmentScheme, MLE},
-        StructuredReferenceString,
-    };
-    use jf_utils::test_rng;
-    use std::time::Instant;
+fn verify(c: &mut Criterion) {
+    let max_num_vars = 18;
+    let nv = 17;
 
-    pub(crate) fn bench_pcs() -> Result<(), PCSError> {
-        let mut rng = test_rng();
+    let mut rng = &mut test_rng();
+    let uni_params =
+        MultilinearKzgPCS::<Bn254>::gen_srs_for_testing(&mut rng, max_num_vars).unwrap();
 
-        // normal polynomials
-        let uni_params = MultilinearKzgPCS::<Bls12_381>::gen_srs_for_testing(&mut rng, 18)?;
+    let (ml_ck, ml_vk) = uni_params.0.trim(nv).unwrap();
+    let (uni_ck, uni_vk) = uni_params.1.trim(nv).unwrap();
+    let ck = (ml_ck, uni_ck);
+    let vk = (ml_vk, uni_vk);
 
-        for nv in 4..19 {
-            let repetition = if nv < 10 {
-                100
-            } else if nv < 20 {
-                50
-            } else {
-                10
-            };
+    let polys = (0..SAMPLES)
+        .map(|_| MLE::from(DenseMultilinearExtension::rand(nv, &mut rng)))
+        .collect::<Vec<_>>();
 
-            let poly = MLE::from(DenseMultilinearExtension::rand(nv, &mut rng));
-            let (ml_ck, ml_vk) = uni_params.0.trim(nv)?;
-            let (uni_ck, uni_vk) = uni_params.1.trim(nv)?;
-            let ck = (ml_ck, uni_ck);
-            let vk = (ml_vk, uni_vk);
+    let commitments: Vec<_> = (0..SAMPLES)
+        .map(|i| MultilinearKzgPCS::commit(&ck, &polys[i]).unwrap())
+        .collect();
 
-            let point: Vec<_> = (0..nv).map(|_| Fr::rand(&mut rng)).collect();
+    let points: Vec<_> = (0..SAMPLES)
+        .map(|_| (0..nv).map(|_| Fr::rand(&mut rng)).collect())
+        .collect();
 
-            // commit
-            let com = {
-                let start = Instant::now();
-                for _ in 0..repetition {
-                    let _commit = MultilinearKzgPCS::commit(&ck, &poly)?;
-                }
+    let (proofs, values): (Vec<_>, Vec<_>) = (0..SAMPLES)
+        .map(|i| MultilinearKzgPCS::open(&ck, &polys[i], &points[i]).unwrap())
+        .unzip();
 
-                println!(
-                    "KZG commit for {} variables: {} ns",
-                    nv,
-                    start.elapsed().as_nanos() / repetition as u128
-                );
-
-                MultilinearKzgPCS::commit(&ck, &poly)?
-            };
-
-            // open
-            let (proof, value) = {
-                let start = Instant::now();
-                for _ in 0..repetition {
-                    let _open = MultilinearKzgPCS::open(&ck, &poly, &point)?;
-                }
-
-                println!(
-                    "KZG open for {} variables: {} ns",
-                    nv,
-                    start.elapsed().as_nanos() / repetition as u128
-                );
-                MultilinearKzgPCS::open(&ck, &poly, &point)?
-            };
-
-            // verify
-            {
-                let start = Instant::now();
-                for _ in 0..repetition {
-                    assert!(MultilinearKzgPCS::verify(
-                        &vk, &com, &point, &value, &proof
-                    )?);
-                }
-                println!(
-                    "KZG verify for {} variables: {} ns",
-                    nv,
-                    start.elapsed().as_nanos() / repetition as u128
-                );
-            }
-
-            println!("====================================");
-        }
-
-        Ok(())
-    }
+    c.bench_function("Multilinear KZG Verify", |b| {
+        let mut i = 0;
+        b.iter(|| {
+            i = (i + 1) % SAMPLES;
+            let _verify =
+                MultilinearKzgPCS::verify(&vk, &commitments[i], &points[i], &values[i], &proofs[i])
+                    .unwrap();
+        })
+    });
 }
+
+criterion_group! {
+    name = pcs_benches;
+    config = Criterion::default();
+    targets =
+        commit,
+        open,
+        verify,
+}
+
+criterion_main!(pcs_benches);
