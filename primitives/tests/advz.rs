5c74b9a0a3cd6909fe9d10cbff61057ff705fb6d
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-10-19 13:32:05 -0400

    feat: Use FFT to encode polynomials in eval form (#385)
    * tidy PhantomData
    
    * eval domain too large by 1: multi_open_rou_eval_domain takes degree, but we give it num_coeffs, which is degree+1
    
    * fix https://github.com/EspressoSystems/jellyfish/issues/313
    
    * use fft to encode polynomials in eval form
    
    * add test to verify a 'namespace' aka polynomial
    
    * tidy tests
    
    * update changelog
    
    * [no ci] add explanatory comment to address https://github.com/EspressoSystems/jellyfish/pull/385#discussion_r1360751399

--- primitives/tests/advz.rs
+++ primitives/tests/advz.rs
@@ -12,11 +12,11 @@ mod vid;
 #[test]
 fn round_trip() {
     // play with these items
-    let vid_sizes = [(2, 3), (5, 9)];
+    let vid_sizes = [(2, 3), (8, 11)];
     let byte_lens = [0, 1, 2, 16, 32, 47, 48, 49, 64, 100, 400];
 
     // more items as a function of the above
-    let supported_degree = vid_sizes.iter().max_by_key(|v| v.0).unwrap().0;
+    let supported_degree = vid_sizes.iter().max_by_key(|v| v.0).unwrap().0 - 1;
     let mut rng = jf_utils::test_rng();
     let srs = UnivariateKzgPCS::<Bls12_381>::gen_srs_for_testing(
         &mut rng,
