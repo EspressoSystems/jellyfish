d7ac08f360881fe9c63597a4afac345e1080b0b7
chancharles92 <chancharles92@gmail.com>
2023-12-07 10:52:10 -0800

    refactor: Parallel optimizations for FK23 and multi-openings (#442)
    * parallelize opening proofs generation
    
    * parallel h_poly computation
    
    * minor
    
    * add cfg for APIs
    
    * fix tests in test-srs
    
    * remove commented code
    
    * address comments
    
    * add a comment

--- primitives/src/pcs/univariate_kzg/mod.rs
+++ primitives/src/pcs/univariate_kzg/mod.rs
@@ -17,6 +17,8 @@ use ark_ec::{
     pairing::Pairing, scalar_mul::variable_base::VariableBaseMSM, AffineRepr, CurveGroup,
 };
 use ark_ff::{FftField, Field, PrimeField};
+#[cfg(not(feature = "seq-fk-23"))]
+use ark_poly::EvaluationDomain;
 use ark_poly::{
     univariate::DensePolynomial, DenseUVPolynomial, Polynomial, Radix2EvaluationDomain,
 };
@@ -332,35 +334,56 @@ impl<E: Pairing> UnivariatePCS for UnivariateKzgPCS<E> {
         num_points: usize,
         domain: &Radix2EvaluationDomain<Self::Evaluation>,
     ) -> Result<Vec<Self::Proof>, PCSError> {
-        #[cfg(feature = "naive-kzg-multi-open")]
+        #[cfg(not(feature = "seq-fk-23"))]
         {
-            use ark_poly::EvaluationDomain;
-            let prover_param = prover_param.borrow(); // needed for Send + Sync
+            let h_poly_timer = start_timer!(|| "compute h_poly");
+            let h_poly = Self::compute_h_poly_parallel(prover_param, &polynomial.coeffs)?;
+            end_timer!(h_poly_timer);
+            let small_domain: Radix2EvaluationDomain<Self::Evaluation> =
+                Radix2EvaluationDomain::new(h_poly.degree() + 1).ok_or_else(|| {
+                    PCSError::InvalidParameters(format!(
+                        "failed to create a domain of size {}",
+                        h_poly.degree() + 1,
+                    ))
+                })?;
+            let parallel_factor = domain.size() / small_domain.size();
 
-            // We prefer use `.par_bridge()` instead of
-            // `.collect::<Vec<_>>().par_iter()`.
-            // (It avoids an unnecessary `collect()`.)
-            // However, `par_iter` is guaranteed to preserve order,
-            // whereas `par_bridge` is not!
-            // https://github.com/rayon-rs/rayon/issues/551#issuecomment-882069261
-            // https://docs.rs/rayon/latest/rayon/iter/trait.ParallelBridge.html
-            //
-            // We prefer to compute only the proof---not the evaluation.
-            // However, `Self::open()` returns both,
-            // so we throw away the eval via `.map(|r| r.0)`.
-            // https://github.com/EspressoSystems/jellyfish/issues/426
-            domain
-                .elements()
-                .take(num_points)
-                .collect::<Vec<_>>()
-                .par_iter()
-                .map(|point| Self::open(prover_param, polynomial, point).map(|r| r.0))
-                .collect()
+            let mut offsets = Vec::with_capacity(parallel_factor);
+            offsets.push(Self::Evaluation::one());
+            for _ in 1..parallel_factor {
+                offsets.push(domain.group_gen() * offsets.last().unwrap());
+            }
+            let proofs_timer = start_timer!(|| format!(
+                "gen eval proofs with parallel_factor {} and num_points {}",
+                parallel_factor, num_points,
+            ));
+            let proofs: Vec<Vec<_>> = parallelizable_slice_iter(&offsets)
+                .map(|&offset| {
+                    small_domain
+                        .get_coset(offset)
+                        .unwrap()
+                        .fft(&h_poly.coeffs[..])
+                })
+                .collect();
+            end_timer!(proofs_timer);
+            let mut res = vec![];
+            for j in 0..small_domain.size() {
+                for proof in proofs.iter() {
+                    res.push(UnivariateKzgProof {
+                        proof: proof[j].into_affine(),
+                    });
+                }
+            }
+            res = res.into_iter().take(num_points).collect();
+            Ok(res)
         }
 
-        #[cfg(not(feature = "naive-kzg-multi-open"))]
+        #[cfg(feature = "seq-fk-23")]
         {
+            let h_poly_timer = start_timer!(|| "compute h_poly");
             let mut h_poly = Self::compute_h_poly_in_fk23(prover_param, &polynomial.coeffs)?;
+            end_timer!(h_poly_timer);
+            let proofs_timer = start_timer!(|| "gen eval proofs");
             let proofs: Vec<_> = h_poly
                 .batch_evaluate_rou(domain)?
                 .into_iter()
@@ -369,6 +392,7 @@ impl<E: Pairing> UnivariatePCS for UnivariateKzgPCS<E> {
                     proof: g.into_affine(),
                 })
                 .collect();
+            end_timer!(proofs_timer);
             Ok(proofs)
         }
     }
@@ -546,6 +570,41 @@ where
     E: Pairing<ScalarField = F>,
     F: FftField,
 {
+    // Computes h_poly as the matrix-vector product on page 3 of https://eprint.iacr.org/2023/033.pdf via naive row-column inner products in parallel
+    #[cfg(not(feature = "seq-fk-23"))]
+    fn compute_h_poly_parallel(
+        prover_param: impl Borrow<UnivariateProverParam<E>>,
+        poly_coeffs: &[E::ScalarField],
+    ) -> Result<GeneralDensePolynomial<E::G1, F>, PCSError> {
+        if poly_coeffs.is_empty() {
+            return Ok(GeneralDensePolynomial::from_coeff_vec(vec![]));
+        }
+        let h_poly_deg = poly_coeffs.len() - 1;
+        let srs_vec: Vec<E::G1Affine> = prover_param
+            .borrow()
+            .powers_of_g
+            .iter()
+            .take(h_poly_deg)
+            .rev()
+            .cloned()
+            .collect();
+
+        let matrix: Vec<Vec<E::ScalarField>> = (0..h_poly_deg)
+            .map(|i| {
+                poly_coeffs
+                    .iter()
+                    .rev()
+                    .take(h_poly_deg - i)
+                    .copied()
+                    .collect()
+            })
+            .collect();
+        let h_vec: Vec<E::G1> = parallelizable_slice_iter(&matrix)
+            .map(|coeffs| E::G1::msm(&srs_vec[h_poly_deg - coeffs.len()..], &coeffs[..]).unwrap())
+            .collect();
+        Ok(GeneralDensePolynomial::from_coeff_vec(h_vec))
+    }
+
     // Sec 2.2. of <https://eprint.iacr.org/2023/033>
     fn compute_h_poly_in_fk23(
         prover_param: impl Borrow<UnivariateProverParam<E>>,
