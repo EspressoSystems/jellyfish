5a2079f61e34fe5d764d34615fa6ae6e5a1c53dd
MRain <linmrain@gmail.com>
2024-01-12 13:12:23 -0500

    fmt

--- primitives/src/signatures/bls_over_bn254.rs
+++ primitives/src/signatures/bls_over_bn254.rs
@@ -561,7 +561,8 @@ mod tests {
                 assert_ne!(vk1.cmp(&vk2), ark_std::cmp::Ordering::Equal);
                 assert_ne!(vk1, vk2);
             }
-            // Generate two group elements that are equal but have different projective representation.
+            // Generate two group elements that are equal but have different projective
+            // representation.
             let mut vk = VerKey::from(&SignKey::generate(&mut rng));
             let vk_copy = vk.clone();
             let scalar = ark_bn254::Fq::rand(&mut rng);
