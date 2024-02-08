944492999c5907860770dc78714ae007f525a41b
Anders Konring <anders.konring@gmail.com>
2024-02-08 17:56:43 +0100

    fix index errors when using multiplicity > 1

--- primitives/tests/advz.rs
+++ primitives/tests/advz.rs
@@ -14,13 +14,14 @@ fn round_trip() {
     // play with these items
     let vid_sizes = [(2, 3), (8, 11)];
     let payload_byte_lens = [0, 1, 2, 16, 32, 47, 48, 49, 64, 100, 400];
+    let multiplicity = 1;
 
     // more items as a function of the above
     let supported_degree = vid_sizes.iter().max_by_key(|v| v.0).unwrap().0 - 1;
     let mut rng = jf_utils::test_rng();
     let srs = UnivariateKzgPCS::<Bls12_381>::gen_srs_for_testing(
         &mut rng,
-        checked_fft_size(supported_degree).unwrap(),
+        checked_fft_size(supported_degree).unwrap() * multiplicity,
     )
     .unwrap();
 
@@ -32,7 +33,13 @@ fn round_trip() {
 
     vid::round_trip(
         |payload_chunk_size, num_storage_nodes| {
-            Advz::<Bls12_381, Sha256>::new(payload_chunk_size, num_storage_nodes, 1, &srs).unwrap()
+            Advz::<Bls12_381, Sha256>::new(
+                payload_chunk_size,
+                num_storage_nodes,
+                multiplicity,
+                &srs,
+            )
+            .unwrap()
         },
         &vid_sizes,
         &payload_byte_lens,
