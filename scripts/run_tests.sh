d6439a3415974ef11b66ca49ee1f1644f2a3bf1b
MRain <linmrain@gmail.com>
2024-02-27 12:35:42 -0500

    temporarily use stable cargo for test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,7 +4,7 @@ set -e
 # We want the code to panic if there is an integer overflow
 export RUSTFLAGS="-C overflow-checks=on"
 
-cargo +nightly test --release -p jf-utils -- -Zunstable-options --report-time
-cargo +nightly test --release -p jf-plonk --lib --bins -- -Zunstable-options --report-time
-cargo +nightly test --release -p jf-primitives --features test-srs -- -Zunstable-options --report-time # enable test-srs feature for gen_srs_for_testing
-cargo +nightly test --release -p jf-relation -- -Zunstable-options --report-time
+cargo test --release -p jf-utils -- -Zunstable-options --report-time
+cargo test --release -p jf-plonk --lib --bins -- -Zunstable-options --report-time
+cargo test --release -p jf-primitives --features test-srs -- -Zunstable-options --report-time # enable test-srs feature for gen_srs_for_testing
+cargo test --release -p jf-relation -- -Zunstable-options --report-time
