819eaf63d4dcf553b0d54ed2ab108203d4f1f284
Alex Xiong <alex.xiong.tech@gmail.com>
2024-03-01 15:00:47 +0000

    adjust benchmark name

--- primitives/benches/kzg_gpu.rs
+++ primitives/benches/kzg_gpu.rs
@@ -1,26 +1,28 @@
 //! This benchmark meant for MSM speed comparison between arkworks and
 //! GPU-accelerated code We use `UnivariateKzgPCS::commit()` as a proxy for MSM
 //!
-//! Run `cargo bench --bench msm --features "test-srs icicle"`
+//! Run `cargo bench --bench kzg-gpu --features "test-srs icicle"`
 use ark_bn254::Bn254;
-#[cfg(feature = "icicle")]
-use ark_ec::models::{short_weierstrass::Affine, CurveConfig};
-use ark_ec::pairing::Pairing;
+use ark_ec::{
+    models::{short_weierstrass::Affine, CurveConfig},
+    pairing::Pairing,
+};
 use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-#[cfg(feature = "icicle")]
-use jf_primitives::icicle_deps::*;
-use jf_primitives::pcs::{
-    prelude::{PolynomialCommitmentScheme, UnivariateKzgPCS},
-    StructuredReferenceString,
+use jf_primitives::{
+    icicle_deps::*,
+    pcs::{
+        prelude::{PolynomialCommitmentScheme, UnivariateKzgPCS},
+        StructuredReferenceString,
+    },
 };
 use jf_utils::test_rng;
 
-const MIN_LOG_DEGREE: usize = 12;
+const MIN_LOG_DEGREE: usize = 19;
 const MAX_LOG_DEGREE: usize = 23;
 
 /// running MSM using arkworks backend
-pub fn bench_msm_ark<E: Pairing>(c: &mut Criterion) {
+pub fn kzg_ark<E: Pairing>(c: &mut Criterion) {
     let mut group = c.benchmark_group("MSM with arkworks");
     let mut rng = test_rng();
 
@@ -46,7 +48,7 @@ pub fn bench_msm_ark<E: Pairing>(c: &mut Criterion) {
 
 /// running MSM using ICICLE backends
 #[cfg(feature = "icicle")]
-pub fn bench_msm_icicle<E, C>(c: &mut Criterion)
+pub fn kzg_icicle<E, C>(c: &mut Criterion)
 where
     C: IcicleCurve + MSM<C>,
     C::ScalarField: ArkConvertible<ArkEquivalent = E::ScalarField>,
@@ -58,6 +60,11 @@ where
 
     let supported_degree = 2usize.pow(MAX_LOG_DEGREE as u32);
     let pp = UnivariateKzgPCS::<E>::gen_srs_for_testing(&mut rng, supported_degree).unwrap();
+    // TODO: (alex) figure out load longer SRS first, and only use part of it later
+    // currently it will error if the `scalars.len() % points.len() != 0`
+    // while we can tap into a slice behind the reference via
+    // `HostOrDeviceSlice[..]` which gives `&[T]` however msm() doesn't accept
+    // &[T], only `&HostOrDeviceSlice`
 
     // setup for commit first
     for log_degree in MIN_LOG_DEGREE..MAX_LOG_DEGREE {
@@ -78,17 +85,16 @@ where
     group.finish();
 }
 
-fn msm_bn254(c: &mut Criterion) {
-    bench_msm_ark::<Bn254>(c);
-    #[cfg(feature = "icicle")]
-    bench_msm_icicle::<Bn254, icicle_bn254::curve::CurveCfg>(c);
+fn kzg_gpu_bn254(c: &mut Criterion) {
+    // kzg_ark::<Bn254>(c);
+    kzg_icicle::<Bn254, icicle_bn254::curve::CurveCfg>(c);
 }
 
 criterion_group! {
-    name = msm_benches;
+    name = kzg_gpu_benches;
     config = Criterion::default().sample_size(10);
     targets =
-        msm_bn254,
+        kzg_gpu_bn254,
 }
 
-criterion_main!(msm_benches);
+criterion_main!(kzg_gpu_benches);
