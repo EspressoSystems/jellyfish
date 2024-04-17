7eab186d75e8a950fa0b58e7dceef8187c3e98e9
MRain <linmrain@gmail.com>
2024-04-17 18:13:33 +0800

    update scripts & github actions

--- scripts/check_no_std.sh
+++ scripts/check_no_std.sh
@@ -4,11 +4,11 @@ set -x
 
 cargo-nono check --no-default-features --package jf-utils
 cargo-nono check --no-default-features --package jf-relation
-cargo-nono check --no-default-features --package jf-primitives
+cargo-nono check --no-default-features --package jf-primitives --features "test-srs, gadgets"
 cargo-nono check --no-default-features --package jf-primitives-core
-cargo-nono check --no-default-features --package jf-merkle-tree
-cargo-nono check --no-default-features --package jf-pcs
-cargo-nono check --no-default-features --package jf-rescue
+cargo-nono check --no-default-features --package jf-merkle-tree --features "gadgets"
+cargo-nono check --no-default-features --package jf-pcs --features "test-srs"
+cargo-nono check --no-default-features --package jf-rescue --features "gadgets"
 cargo-nono check --no-default-features --package jf-signature --features "bls, schnorr, gadgets"
 cargo-nono check --no-default-features --package jf-vdf
-cargo-nono check --no-default-features --package jf-vid
+cargo-nono check --no-default-features --package jf-vid --features "test-srs"
--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -11,7 +11,7 @@ export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=3 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
 export RUSTDOCFLAGS=""
 rm -vf ./target/**/*.gcda
-cargo build
-cargo test --lib
+cargo build --features "test-srs, gadgets, bls, schnorr"
+cargo test --lib --features "test-srs, gadgets, bls, schnorr"
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing  $IGNORED_FILES -o ./target/debug/coverage/
 echo "Coverage report available at target/debug/coverage/index.html."
