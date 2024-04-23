f8b6d0cd2c3bae9b9c2cd0951cb7395e685122f9
MRain <linmrain@gmail.com>
2024-04-23 16:27:58 +0800

    update scripts

--- scripts/check_no_std.sh
+++ scripts/check_no_std.sh
@@ -5,7 +5,6 @@ set -x
 # WARN: cargo-nono is reporting false positives.
 cargo-nono check --no-default-features --package jf-utils
 cargo-nono check --no-default-features --package jf-relation
-cargo-nono check --no-default-features --package jf-primitives --features "test-srs, gadgets"
 cargo-nono check --no-default-features --package jf-traits
 cargo-nono check --no-default-features --package jf-merkle-tree --features "gadgets"
 cargo-nono check --no-default-features --package jf-pcs --features "test-srs"
@@ -13,3 +12,6 @@ cargo-nono check --no-default-features --package jf-rescue --features "gadgets"
 cargo-nono check --no-default-features --package jf-signature --features "bls, schnorr, gadgets"
 cargo-nono check --no-default-features --package jf-vdf
 cargo-nono check --no-default-features --package jf-vid --features "test-srs"
+cargo-nono check --no-default-features --package jf-aead
+cargo-nono check --no-default-features --package jf-elgamal --features "gadgets"
+cargo-nono check --no-default-features --package jf-vrf
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,7 +6,6 @@ export RUSTFLAGS="-C overflow-checks=on"
 
 cargo test --release -p jf-utils #-- -Zunstable-options --report-time
 cargo test --release -p jf-plonk --lib --bins #-- -Zunstable-options --report-time
-cargo test --release -p jf-primitives --features "test-srs, gadgets" #-- -Zunstable-options --report-time # enable test-srs feature for gen_srs_for_testing
 cargo test --release -p jf-traits #-- -Zunstable-options --report-time
 cargo test --release -p jf-merkle-tree --features gadgets #-- -Zunstable-options --report-time
 cargo test --release -p jf-pcs --features test-srs #-- -Zunstable-options --report-time
@@ -14,3 +13,6 @@ cargo test --release -p jf-rescue --features gadgets #-- -Zunstable-options --re
 cargo test --release -p jf-signature --features "bls, schnorr, gadgets" #-- -Zunstable-options --report-time
 cargo test --release -p jf-vdf #-- -Zunstable-options --report-time
 cargo test --release -p jf-vid --features test-srs #-- -Zunstable-options --report-time
+cargo test --release -p jf-aead #-- -Zunstable-options --report-time
+cargo test --release -p jf-elgamal --features gadgets #-- -Zunstable-options --report-time
+cargo test --release -p jf-vrf #-- -Zunstable-options --report-time
