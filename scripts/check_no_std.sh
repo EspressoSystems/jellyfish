04200fbb13416853bef6090eea99c51dfca22d20
Chengyu Lin <linmrain@gmail.com>
2024-09-18 09:41:46 -0400

    revert!: deprecate VDF crate (#683)
    * deprecate minroot & add hashchain
    
    * changelog
    
    * fix stupid bug & ark-serialize derive
    
    * deprecate VDF crate

--- scripts/check_no_std.sh
+++ scripts/check_no_std.sh
@@ -9,7 +9,6 @@ cargo-nono check --no-default-features --package jf-merkle-tree --features "gadg
 cargo-nono check --no-default-features --package jf-pcs --features "test-srs"
 cargo-nono check --no-default-features --package jf-rescue --features "gadgets"
 cargo-nono check --no-default-features --package jf-signature --features "bls, schnorr, gadgets"
-cargo-nono check --no-default-features --package jf-vdf
 cargo-nono check --no-default-features --package jf-vid --features "test-srs"
 cargo-nono check --no-default-features --package jf-aead
 cargo-nono check --no-default-features --package jf-elgamal --features "gadgets"
