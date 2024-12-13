ac97cb29198c611ade2b75ea966d0bc121a43983
Alex Xiong <alex.xiong.tech@gmail.com>
2024-12-13 21:46:35 +0800

    add concrete sponge functions and tests

--- poseidon2/src/sponge.rs
+++ poseidon2/src/sponge.rs
@@ -99,3 +99,99 @@ where
         self.state.zeroize();
     }
 }
+
+#[cfg(feature = "bls12-381")]
+mod bls12_381 {
+    #![allow(dead_code)]
+    use super::*;
+    use crate::constants::bls12_381::*;
+    use ark_bls12_381::Fr;
+    use nimue::hash::sponge::DuplexSponge;
+    /// A sponge over BLS12-381 scalar field, state_size=2, rate=1.
+    pub type Poseidon2SpongeBlsN2R1 = DuplexSponge<Poseidon2Sponge<Fr, 2, 1, Poseidon2ParamsBls2>>;
+    /// A sponge over BLS12-381 scalar field, state_size=3, rate=1.
+    pub type Poseidon2SpongeBlsN3R1 = DuplexSponge<Poseidon2Sponge<Fr, 3, 1, Poseidon2ParamsBls3>>;
+    /// A sponge over BLS12-381 scalar field, state_size=3, rate=2.
+    pub type Poseidon2SpongeBlsN3R2 = DuplexSponge<Poseidon2Sponge<Fr, 3, 2, Poseidon2ParamsBls3>>;
+
+    #[test]
+    fn test_bls_sponge() {
+        use super::tests::test_sponge;
+        test_sponge::<Fr, Poseidon2SpongeBlsN2R1>();
+        test_sponge::<Fr, Poseidon2SpongeBlsN3R1>();
+        test_sponge::<Fr, Poseidon2SpongeBlsN3R2>();
+    }
+}
+
+#[cfg(feature = "bn254")]
+mod bn254 {
+    #![allow(dead_code)]
+    use super::*;
+    use crate::constants::bn254::*;
+    use ark_bn254::Fr;
+    use nimue::hash::sponge::DuplexSponge;
+    /// A sponge over BN254 scalar field, state_size=3, rate=1.
+    pub type Poseidon2SpongeBnN3R1 = DuplexSponge<Poseidon2Sponge<Fr, 3, 1, Poseidon2ParamsBn3>>;
+    /// A sponge over BN254 scalar field, state_size=3, rate=2.
+    pub type Poseidon2SpongeBnN3R2 = DuplexSponge<Poseidon2Sponge<Fr, 3, 2, Poseidon2ParamsBn3>>;
+
+    #[test]
+    fn test_bn_sponge() {
+        use super::tests::test_sponge;
+        test_sponge::<Fr, Poseidon2SpongeBnN3R1>();
+        test_sponge::<Fr, Poseidon2SpongeBnN3R2>();
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+    use ark_ff::BigInteger;
+    use ark_std::vec::Vec;
+    use nimue::{DuplexHash, IOPattern, UnitTranscript};
+
+    // inspired by:
+    // <https://github.com/arkworks-rs/nimue/blob/bdec8c446b804930a8375a8d2a3703a6071abf6b/nimue-poseidon/src/tests.rs#L16C4-L16C44>
+    pub(crate) fn test_sponge<F: PrimeField + Unit, H: DuplexHash<F>>() {
+        let io = IOPattern::<H, F>::new("test")
+            .absorb(1, "in")
+            .squeeze(2048, "out");
+
+        // prover transcript
+        let mut merlin = io.to_merlin();
+        // prover first message (label: "in")
+        merlin.add_units(&[F::from(42u32)]).unwrap();
+
+        let mut merlin_challenges = [F::default(); 2048];
+        merlin.fill_challenge_units(&mut merlin_challenges).unwrap();
+
+        // verifier transcript
+        let mut arthur = io.to_arthur(merlin.transcript());
+        // reading prover's first message labelled "in", since we don't need it, read
+        // into a one-time throw-away array
+        arthur.fill_next_units(&mut [F::default()]).unwrap();
+        let mut arthur_challenges = [F::default(); 2048];
+        arthur.fill_challenge_units(&mut arthur_challenges).unwrap();
+
+        // challenge computed from both sides should be the same
+        assert_eq!(merlin_challenges, arthur_challenges);
+
+        // Looking at byte distribution, whether it's close to uniform
+        let chal_bytes: Vec<u8> = merlin_challenges
+            .iter()
+            .flat_map(|c| c.into_bigint().to_bytes_le())
+            .collect();
+        let frequencies = (0u8..=255)
+            .map(|i| chal_bytes.iter().filter(|&&x| x == i).count())
+            .collect::<Vec<_>>();
+        // the expected frequency if it's uniformly random
+        let expected_mean = (F::MODULUS_BIT_SIZE / 8 * 2048 / 256) as usize;
+        assert!(
+            frequencies
+                .iter()
+                .all(|&x| x < expected_mean * 2 && x > expected_mean / 2),
+            "Counts for each value shouldn't be too far away from mean: {:?}",
+            frequencies
+        );
+    }
+}
