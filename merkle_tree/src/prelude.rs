02ef737b278042f01dc6e59e36dc74eb0389957f
itsasecret <87604944+tudorpintea999@users.noreply.github.com>
2024-12-14 21:52:43 +0200

    Update prelude.rs
    Simplified and structured implementation of key components.
    Unified formatting and comments for better understanding.
    	 Centralized error propagation for digest functions.
    	Streamlined macro logic for implementing 32-byte hash functions.
    	 Logical grouping and type aliasing for better usability.

--- merkle_tree/src/prelude.rs
+++ merkle_tree/src/prelude.rs
@@ -4,7 +4,7 @@
 // You should have received a copy of the MIT License
 // along with the Jellyfish library. If not, see <https://mit-license.org/>.
 
-//! Prelude. Also provides sample instantiations of merkle trees.
+//! Prelude module providing essential Merkle tree types and sample implementations.
 
 pub use crate::{
     append_only::MerkleTree,
@@ -16,8 +16,8 @@ pub use crate::{
     ToTraversalPath, UniversalMerkleTreeScheme,
 };
 
-use super::light_weight::LightWeightMerkleTree;
 use crate::errors::MerkleTreeError;
+use super::light_weight::LightWeightMerkleTree;
 use ark_ff::PrimeField;
 use ark_serialize::{
     CanonicalDeserialize, CanonicalSerialize, Compress, Read, SerializationError, Valid, Validate,
@@ -28,38 +28,37 @@ use jf_poseidon2::{Poseidon2, Poseidon2Params};
 use jf_rescue::{crhf::RescueCRHF, RescueParameter};
 use sha3::{Digest, Keccak256, Sha3_256};
 
-/// Wrapper for rescue hash function
+/// Wrapper for the Rescue hash function.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct RescueHash<F: RescueParameter> {
-    phantom_f: PhantomData<F>,
+    _phantom: PhantomData<F>,
 }
 
 impl<I: Index, F: RescueParameter + From<I>> DigestAlgorithm<F, I, F> for RescueHash<F> {
     fn digest(data: &[F]) -> Result<F, MerkleTreeError> {
-        Ok(RescueCRHF::<F>::sponge_no_padding(data, 1)?[0])
+        RescueCRHF::<F>::sponge_no_padding(data, 1)
+            .map(|result| result[0])
+            .map_err(MerkleTreeError::from)
     }
 
     fn digest_leaf(pos: &I, elem: &F) -> Result<F, MerkleTreeError> {
-        let data = [F::zero(), F::from(pos.clone()), *elem];
-        Ok(RescueCRHF::<F>::sponge_no_padding(&data, 1)?[0])
+        let input = [F::zero(), F::from(pos.clone()), *elem];
+        Self::digest(&input)
     }
 }
 
-/// A standard merkle tree using RATE-3 rescue hash function
+/// A standard Merkle tree using RATE-3 Rescue hash function.
 pub type RescueMerkleTree<F> = MerkleTree<F, RescueHash<F>, u64, 3, F>;
 
-/// A standard light merkle tree using RATE-3 rescue hash function
+/// A standard lightweight Merkle tree using RATE-3 Rescue hash function.
 pub type RescueLightWeightMerkleTree<F> = LightWeightMerkleTree<F, RescueHash<F>, u64, 3, F>;
 
-/// Example instantiation of a SparseMerkleTree indexed by I
+/// A Sparse Merkle tree indexed by `I`, using Rescue hash function.
 pub type RescueSparseMerkleTree<I, F> = UniversalMerkleTree<F, RescueHash<F>, I, 3, F>;
 
-// TODO: (alex) move this compression to CRHF and wrap with better API?
-/// Wrapper for Poseidon2 compression function
+/// Wrapper for the Poseidon2 compression function.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Poseidon2Compression<F, P, const N: usize>(
-    (PhantomData<F>, PhantomData<P>, PhantomData<[(); N]>),
-)
+pub struct Poseidon2Compression<F, P, const N: usize>(PhantomData<(F, P, [(); N])>)
 where
     F: PrimeField,
     P: Poseidon2Params<F, N>;
@@ -84,33 +83,19 @@ where
     }
 }
 
-/// Implement Internal node type and implement DigestAlgorithm for a hash
-/// function with 32 bytes output size
-///
-/// # Usage
-/// `impl_mt_hash_256!(Sha3_256, Sha3Node, Sha3Digest)` will
-/// - introduce `struct Sha3Node` for internal node in a merkle tree that use
-///   Sha3_256 as hash
-/// - introduce `struct Sha3Digest` which implements `DigestAlgorithm`
+/// Implements internal node types and `DigestAlgorithm` for 32-byte hash functions.
 macro_rules! impl_mt_hash_256 {
     ($hasher:ident, $node_name:ident, $digest_name:ident) => {
-        /// Internal node for merkle tree
-        #[derive(Default, Eq, PartialEq, Clone, Copy, Ord, PartialOrd, Hash)]
+        /// Internal node for Merkle tree.
+        #[derive(Default, Clone, Copy, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
         pub struct $node_name(pub(crate) [u8; 32]);
-
-        impl fmt::Debug for $node_name {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                f.debug_tuple(&stringify!($node_name))
-                    .field(&hex::encode(self.0))
-                    .finish()
-            }
-        }
 
         impl AsRef<[u8]> for $node_name {
             fn as_ref(&self) -> &[u8] {
                 &self.0
             }
         }
+
         impl CanonicalSerialize for $node_name {
             fn serialize_with_mode<W: Write>(
                 &self,
@@ -120,62 +105,67 @@ macro_rules! impl_mt_hash_256 {
                 writer.write_all(&self.0)?;
                 Ok(())
             }
+
             fn serialized_size(&self, _compress: Compress) -> usize {
                 32
             }
         }
+
         impl CanonicalDeserialize for $node_name {
             fn deserialize_with_mode<R: Read>(
                 mut reader: R,
                 _compress: Compress,
                 _validate: Validate,
             ) -> Result<Self, SerializationError> {
-                let mut ret = [0u8; 32];
-                reader.read_exact(&mut ret)?;
-                Ok(Self(ret))
+                let mut buffer = [0u8; 32];
+                reader.read_exact(&mut buffer)?;
+                Ok(Self(buffer))
             }
         }
+
         impl Valid for $node_name {
             fn check(&self) -> Result<(), SerializationError> {
                 Ok(())
             }
         }
 
-        /// Wrapper for the actual hash function
-        #[derive(Clone, Debug, Hash, Eq, PartialEq)]
+        /// Digest implementation using the given hash function.
+        #[derive(Debug, Clone, Hash, Eq, PartialEq)]
         pub struct $digest_name;
+
         impl<E: Element + CanonicalSerialize, I: Index> DigestAlgorithm<E, I, $node_name>
             for $digest_name
         {
             fn digest(data: &[$node_name]) -> Result<$node_name, MerkleTreeError> {
-                let mut h = $hasher::new();
-                for value in data {
-                    h.update(value);
+                let mut hasher = $hasher::new();
+                for node in data {
+                    hasher.update(node.as_ref());
                 }
-                Ok($node_name(h.finalize().into()))
+                Ok($node_name(hasher.finalize().into()))
             }
 
             fn digest_leaf(_pos: &I, elem: &E) -> Result<$node_name, MerkleTreeError> {
-                let mut writer = Vec::new();
-                elem.serialize_compressed(&mut writer).unwrap();
-                let mut h = $hasher::new();
-                h.update(writer);
-                Ok($node_name(h.finalize().into()))
+                let mut buffer = Vec::new();
+                elem.serialize_compressed(&mut buffer).unwrap();
+                let mut hasher = $hasher::new();
+                hasher.update(&buffer);
+                Ok($node_name(hasher.finalize().into()))
             }
         }
     };
 }
 
+// Implement hash functions using the macro.
 impl_mt_hash_256!(Sha3_256, Sha3Node, Sha3Digest);
 impl_mt_hash_256!(Keccak256, Keccak256Node, Keccak256Digest);
 
-/// Merkle tree using SHA3 hash
+/// A Merkle tree using SHA3 hash function.
 pub type SHA3MerkleTree<E> = MerkleTree<E, Sha3Digest, u64, 3, Sha3Node>;
-/// Light weight merkle tree using SHA3 hash
+/// A lightweight Merkle tree using SHA3 hash function.
 pub type LightWeightSHA3MerkleTree<E> = LightWeightMerkleTree<E, Sha3Digest, u64, 3, Sha3Node>;
 
-/// Merkle tree using keccak256 hash
-pub type Keccak256MerkleTree<E> = MerkleTree<E, Keccak256Node, u64, 3, Keccak256Digest>;
-/// Light weight merkle tree using Keccak256 hash
+/// A Merkle tree using Keccak256 hash function.
+pub type Keccak256MerkleTree<E> = MerkleTree<E, Keccak256Digest, u64, 3, Keccak256Node>;
+/// A lightweight Merkle tree using Keccak256 hash function.
 pub type LightWeightKeccak256MerkleTree<E> =
     LightWeightMerkleTree<E, Keccak256Digest, u64, 3, Keccak256Node>;
