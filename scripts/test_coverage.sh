f8fa50dbfe96adda6e32f6383802448a1158fcd6
Chengyu Lin <linmrain@gmail.com>
2024-12-13 21:31:04 -0500

    pin the nightly toolchain (#711)

--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -10,7 +10,7 @@ export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=3 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
 export RUSTDOCFLAGS=""
 rm -vf ./target/**/*.gcda
-cargo +nightly build --features "test-srs, gadgets, bls, schnorr"
-cargo +nightly test --lib --features "test-srs, gadgets, bls, schnorr"
+cargo +nightly-2024-06-25 build --features "test-srs, gadgets, bls, schnorr"
+cargo +nightly-2024-06-25 test --lib --features "test-srs, gadgets, bls, schnorr"
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing  $IGNORED_FILES -o ./target/debug/coverage/
 echo "Coverage report available at target/debug/coverage/index.html."
