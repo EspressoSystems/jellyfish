2d2d1fb2f9c148f4c32229ba76a3b8a07ed4baff
Ancient123 <Ancient123@users.noreply.github.com>
2025-06-16 00:23:51 +0000

    deploy: 4ba71c5d381667ab7c02e64537494502b3e582ea

--- trait.impl/jf_prf/trait.PRF.js
+++ trait.impl/jf_prf/trait.PRF.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["jf_rescue",[["impl&lt;F: <a class=\"trait\" href=\"jf_rescue/trait.RescueParameter.html\" title=\"trait jf_rescue::RescueParameter\">RescueParameter</a>, const INPUT_LEN: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.usize.html\">usize</a>, const OUTPUT_LEN: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.usize.html\">usize</a>&gt; PRF for <a class=\"struct\" href=\"jf_rescue/prf/struct.RescuePRF.html\" title=\"struct jf_rescue::prf::RescuePRF\">RescuePRF</a>&lt;F, INPUT_LEN, OUTPUT_LEN&gt;"]]]]);
+    var implementors = Object.fromEntries([["jf_rescue",[["impl&lt;F: <a class=\"trait\" href=\"jf_rescue/trait.RescueParameter.html\" title=\"trait jf_rescue::RescueParameter\">RescueParameter</a>, const INPUT_LEN: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.usize.html\">usize</a>, const OUTPUT_LEN: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"jf_prf/trait.PRF.html\" title=\"trait jf_prf::PRF\">PRF</a> for <a class=\"struct\" href=\"jf_rescue/prf/struct.RescuePRF.html\" title=\"struct jf_rescue::prf::RescuePRF\">RescuePRF</a>&lt;F, INPUT_LEN, OUTPUT_LEN&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[561]}
+//{"start":57,"fragment_lengths":[643]}
