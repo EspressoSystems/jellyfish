dd0fda10b519c264f7b88ab3d46c9f04f0f5ad09
Alex Xiong <alex.xiong.tech@gmail.com>
2025-09-09 22:00:12 +0800

    evoke jf-vid member crate from workspace

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -10,7 +10,6 @@ cargo +nightly-2025-09-04 test --release -p jf-merkle-tree --features gadgets --
 cargo +nightly-2025-09-04 test --release -p jf-pcs --features test-srs -- -Zunstable-options --report-time
 cargo +nightly-2025-09-04 test --release -p jf-rescue --features gadgets -- -Zunstable-options --report-time
 cargo +nightly-2025-09-04 test --release -p jf-signature --features "bls, schnorr, gadgets" -- -Zunstable-options --report-time
-cargo +nightly-2025-09-04 test --release -p jf-vid --features test-srs -- -Zunstable-options --report-time
 cargo +nightly-2025-09-04 test --release -p jf-aead -- -Zunstable-options --report-time
 cargo +nightly-2025-09-04 test --release -p jf-elgamal --features gadgets -- -Zunstable-options --report-time
 cargo +nightly-2025-09-04 test --release -p jf-vrf -- -Zunstable-options --report-time
