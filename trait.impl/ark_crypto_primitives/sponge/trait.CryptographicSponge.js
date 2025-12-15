e3e9b50ae8722ca97de6a01b95a2f36e5e6a9831
Ancient123 <Ancient123@users.noreply.github.com>
2025-12-15 00:27:15 +0000

    deploy: 06ad78605b5524b60dd546ce90a15f6bfb546f9d

--- trait.impl/ark_crypto_primitives/sponge/trait.CryptographicSponge.js
+++ trait.impl/ark_crypto_primitives/sponge/trait.CryptographicSponge.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["jf_rescue",[["impl&lt;T: <a class=\"trait\" href=\"jf_rescue/trait.RescueParameter.html\" title=\"trait jf_rescue::RescueParameter\">RescueParameter</a> + PrimeField, const RATE: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.usize.html\">usize</a>&gt; CryptographicSponge for <a class=\"struct\" href=\"jf_rescue/sponge/struct.RescueSponge.html\" title=\"struct jf_rescue::sponge::RescueSponge\">RescueSponge</a>&lt;T, RATE&gt;"]]]]);
+    var implementors = Object.fromEntries([["jf_rescue",[["impl&lt;T: <a class=\"trait\" href=\"jf_rescue/trait.RescueParameter.html\" title=\"trait jf_rescue::RescueParameter\">RescueParameter</a> + PrimeField, const RATE: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.92.0/std/primitive.usize.html\">usize</a>&gt; CryptographicSponge for <a class=\"struct\" href=\"jf_rescue/sponge/struct.RescueSponge.html\" title=\"struct jf_rescue::sponge::RescueSponge\">RescueSponge</a>&lt;T, RATE&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/ark_crypto_primitives/sponge/trait.SpongeExt.js
+++ trait.impl/ark_crypto_primitives/sponge/trait.SpongeExt.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["jf_rescue",[["impl&lt;F: <a class=\"trait\" href=\"jf_rescue/trait.RescueParameter.html\" title=\"trait jf_rescue::RescueParameter\">RescueParameter</a>, const RATE: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.usize.html\">usize</a>&gt; SpongeExt for <a class=\"struct\" href=\"jf_rescue/sponge/struct.RescueSponge.html\" title=\"struct jf_rescue::sponge::RescueSponge\">RescueSponge</a>&lt;F, RATE&gt;"]]]]);
+    var implementors = Object.fromEntries([["jf_rescue",[["impl&lt;F: <a class=\"trait\" href=\"jf_rescue/trait.RescueParameter.html\" title=\"trait jf_rescue::RescueParameter\">RescueParameter</a>, const RATE: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.92.0/std/primitive.usize.html\">usize</a>&gt; SpongeExt for <a class=\"struct\" href=\"jf_rescue/sponge/struct.RescueSponge.html\" title=\"struct jf_rescue::sponge::RescueSponge\">RescueSponge</a>&lt;F, RATE&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
